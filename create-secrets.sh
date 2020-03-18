001f2021d2d413efc1952d91f122fb97878154c4
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-03-18 13:54:41 -0300

    Reset folder structure

--- create-secrets.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-# This script is used to create a Kubernetes secret with name 'aws-secrets' based on existing
-# environment variables: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, and AWS_ACCOUNT.
-# This Secret will be used by the cloudsim server.
-
-kubectl create secret generic aws-secrets --from-literal=aws-access-key-id=${AWS_ACCESS_KEY_ID} --from-literal=aws-secret-access-key=${AWS_SECRET_ACCESS_KEY} --from-literal=aws-account=${AWS_ACCOUNT} --from-literal=aws-region=${AWS_REGION}
