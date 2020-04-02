b444f0c9f3743f8e4a497b993058575885d41c84
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-04-02 17:25:54 -0300

    Move platform container to simulator package

--- pkg/simulator/node.go
+++ pkg/simulator/node.go
@@ -2,6 +2,13 @@ package simulator
 
 import "time"
 
+const (
+	NODE_NAME_SERVER = "gzserver-container"
+	NODE_NAME_BRIDGE = "comms-bridge"
+	NODE_NAME_FIELD_COMPUTER = "field-computer"
+	NODE_NAME_SIDECAR = "copy-to-s3"
+)
+
 // Node represents a machine that will be used to run a simulation
 type Node struct {
 	ID        uint       `gorm:"primary_key" json:"-"`
