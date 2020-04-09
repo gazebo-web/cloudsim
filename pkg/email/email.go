c8e72df227434720a2ab8bde67547fddf928e402
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-04-09 14:30:58 -0300

    Reduce amount of files on email package

--- pkg/email/email.go
+++ pkg/email/email.go
@@ -12,10 +12,41 @@ type Email struct {
 }
 
 // TODO: Find a better name. This should be its own service and not only a configuration parser.
-// New returns a new Email configuration.
+// New returns a new Email service.
 func New() *Email {
 	email := Email{}
 	email.DefaultEmailRecipients = tools.EnvVarToSlice("IGN_DEFAULT_EMAIL_RECIPIENT")
 	email.DefaultEmailSender, _ = ign.ReadEnvVar("IGN_DEFAULT_EMAIL_SENDER")
 	return &email
+}
+
+// Send sends an email to a specific recipient. If the recipient is nil,
+// then the default recipient defined in the IGN_FLAGS_EMAIL_TO env var will be
+// used.
+func (e *Email) Send(recipient *[]string, sender *string, subject string, templateFilename string, templateData interface{}) *ign.ErrMsg {
+	if recipient == nil {
+		recipient = &e.DefaultEmailRecipients
+	}
+	if sender == nil {
+		sender = &e.DefaultEmailSender
+	}
+	// If the sender or recipient are not defined, then don't send the email
+	if (recipient != nil && len(*recipient) == 0) || (sender != nil && *sender == "") {
+		return nil
+	}
+
+	// Prepare the template
+	content, err := ign.ParseHTMLTemplate(templateFilename, templateData)
+	if err != nil {
+		return ign.NewErrorMessageWithBase(ign.ErrorUnexpected, err)
+	}
+
+	// Send the email
+	for _, r := range *recipient {
+		err = ign.SendEmail(*sender, r, subject, content)
+		if err != nil {
+			return ign.NewErrorMessageWithBase(ign.ErrorUnexpected, err)
+		}
+	}
+	return nil
 }
