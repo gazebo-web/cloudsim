c11a2be30bdb96c0cb3f628c00e42db62f1f02c2
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-04-20 18:31:59 -0300

    Add queue controller

---
+++ pkg/queue/queue_controller.go
@@ -0,0 +1,84 @@
+package queue
+
+import (
+	"context"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/users"
+	"gitlab.com/ignitionrobotics/web/cloudsim/tools"
+	fuel "gitlab.com/ignitionrobotics/web/fuelserver/bundles/users"
+	"gitlab.com/ignitionrobotics/web/ign-go"
+)
+
+type IController interface {
+	GetAll(ctx context.Context, user *fuel.User, page, perPage *int) ([]interface{}, *ign.ErrMsg)
+	Count(ctx context.Context, user *fuel.User) (interface{}, *ign.ErrMsg)
+	MoveToFront(ctx context.Context, user *fuel.User, groupID string) (interface{}, *ign.ErrMsg)
+	MoveToBack(ctx context.Context, user *fuel.User, groupID string) (interface{}, *ign.ErrMsg)
+	Swap(ctx context.Context, user *fuel.User, groupIDA, groupIDB string) (interface{}, *ign.ErrMsg)
+	Remove(ctx context.Context, user *fuel.User, groupID string) (interface{}, *ign.ErrMsg)
+}
+
+type Controller struct {
+	services services
+}
+
+type services struct {
+	user  *users.Service
+	queue IQueue
+}
+
+// GetAll returns a paginated list of elements from the queue.
+// If no page or perPage arguments are passed, it sets those value to 0 and 10 respectively.
+func (c *Controller) GetAll(ctx context.Context, user *fuel.User, page, perPage *int) ([]interface{}, *ign.ErrMsg) {
+	if ok := c.services.user.Accessor.IsSystemAdmin(*user.Name); !ok {
+		return nil, ign.NewErrorMessage(ign.ErrorUnauthorized)
+	}
+	if page == nil {
+		page = tools.Intptr(0)
+	}
+	if perPage == nil {
+		perPage = tools.Intptr(10)
+	}
+	offset := *page * *perPage
+	limit := *perPage
+	return c.services.queue.Get(&offset, &limit)
+}
+
+// Count returns the element count from the queue.
+func (c *Controller) Count(ctx context.Context, user *fuel.User) (interface{}, *ign.ErrMsg) {
+	if ok := c.services.user.Accessor.IsSystemAdmin(*user.Name); !ok {
+		return nil, ign.NewErrorMessage(ign.ErrorUnauthorized)
+	}
+	return c.services.queue.Count(), nil
+}
+
+// MoveToFront moves an element by the given groupID to the front of the queue.
+func (c *Controller) MoveToFront(ctx context.Context, user *fuel.User, groupID string) (interface{}, *ign.ErrMsg) {
+	if ok := c.services.user.Accessor.IsSystemAdmin(*user.Name); !ok {
+		return nil, ign.NewErrorMessage(ign.ErrorUnauthorized)
+	}
+	return c.services.queue.MoveToFront(groupID)
+}
+
+// MoveToBack moves an element by the given groupID to the back of the queue.
+func (c *Controller) MoveToBack(ctx context.Context, user *fuel.User, groupID string) (interface{}, *ign.ErrMsg) {
+	if ok := c.services.user.Accessor.IsSystemAdmin(*user.Name); !ok {
+		return nil, ign.NewErrorMessage(ign.ErrorUnauthorized)
+	}
+	return c.services.queue.MoveToBack(groupID)
+}
+
+// Swap swaps positions of groupIDs A and B.
+func (c *Controller) Swap(ctx context.Context, user *fuel.User, groupIDA, groupIDB string) (interface{}, *ign.ErrMsg) {
+	if ok := c.services.user.Accessor.IsSystemAdmin(*user.Name); !ok {
+		return nil, ign.NewErrorMessage(ign.ErrorUnauthorized)
+	}
+	return c.services.queue.Swap(groupIDA, groupIDB)
+}
+
+// Remove removes an element by the given groupID from the queue.
+func (c *Controller) Remove(ctx context.Context, user *fuel.User, groupID string) (interface{}, *ign.ErrMsg) {
+	if ok := c.services.user.Accessor.IsSystemAdmin(*user.Name); !ok {
+		return nil, ign.NewErrorMessage(ign.ErrorUnauthorized)
+	}
+	return c.services.queue.Remove(groupID)
+}
