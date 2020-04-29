0c0b23870b9a828e66cd9600b95fd1246d1ece0b
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-04-29 10:17:43 -0300

    Add remove element from queue integration test

--- pkg/queue/queue_test.go
+++ pkg/queue/queue_test.go
@@ -13,6 +13,7 @@ import (
 	"testing"
 )
 
+
 func TestIntegration_GetAll(t *testing.T) {
 	userService := users.NewUserServiceMock()
 
@@ -52,7 +53,7 @@ func TestIntegration_GetAll(t *testing.T) {
 		body, _ := json.Marshal(result)
 		writer.Write(body)
 		writer.WriteHeader(http.StatusOK)
-	})
+	}).Methods(http.MethodGet)
 
 	req, err := http.NewRequest(http.MethodGet, "/queue", nil)
 	if err != nil {
@@ -68,6 +69,8 @@ func TestIntegration_GetAll(t *testing.T) {
 	assert.Equal(t, http.StatusOK, recorder.Code)
 	assert.Len(t, response, 3)
 }
+
+
 
 func TestIntegration_MoveToFront(t *testing.T) {
 	userService := users.NewUserServiceMock()
@@ -104,7 +107,7 @@ func TestIntegration_MoveToFront(t *testing.T) {
 		body, _ := json.Marshal(result)
 		writer.Write(body)
 		writer.WriteHeader(http.StatusOK)
-	}).Methods("PATCH")
+	}).Methods(http.MethodPatch)
 
 	queue.Enqueue("1")
 	queue.Enqueue("2")
@@ -129,6 +132,8 @@ func TestIntegration_MoveToFront(t *testing.T) {
 	assert.True(t, ok)
 	assert.Equal(t, response, casted)
 }
+
+
 
 func TestIntegration_MoveToBack(t *testing.T) {
 	userService := users.NewUserServiceMock()
@@ -165,7 +170,7 @@ func TestIntegration_MoveToBack(t *testing.T) {
 		body, _ := json.Marshal(result)
 		writer.Write(body)
 		writer.WriteHeader(http.StatusOK)
-	}).Methods("PATCH")
+	}).Methods(http.MethodPatch)
 
 	queue.Enqueue("1")
 	queue.Enqueue("2")
@@ -190,6 +195,8 @@ func TestIntegration_MoveToBack(t *testing.T) {
 	assert.True(t, ok)
 	assert.Equal(t, response, casted)
 }
+
+
 
 func TestIntegration_Count(t *testing.T) {
 	userService := users.NewUserServiceMock()
@@ -226,7 +233,7 @@ func TestIntegration_Count(t *testing.T) {
 		body, _ := json.Marshal(result)
 		writer.Write(body)
 		writer.WriteHeader(http.StatusOK)
-	}).Methods("PATCH")
+	}).Methods(http.MethodPatch)
 
 	queue.Enqueue("1")
 	queue.Enqueue("2")
@@ -249,5 +256,63 @@ func TestIntegration_Count(t *testing.T) {
 	assert.Equal(t, count, response)
 }
 
+
+
 func TestIntegration_Remove(t *testing.T) {
+	userService := users.NewUserServiceMock()
+
+	adminUsername := "root"
+	admin := fuel.User{
+		Name:             tools.Sptr("Admin Root"),
+		Username:         &adminUsername,
+		Email:            tools.Sptr("root@admin.com"),
+	}
+
+	userService.GetUserFromUsernameMock = func(username string) (user *fuel.User, msg *ign.ErrMsg) {
+		return &admin, nil
+	}
+
+	userService.IsSystemAdminMock = func(user string) bool {
+		return user == adminUsername
+	}
+
+	queue := NewQueue()
+	service := NewService(queue, userService)
+	controller := NewController(service)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/queue/{groupID}", func(writer http.ResponseWriter, request *http.Request) {
+		result, err := controller.Remove(&admin, writer, request)
+		if err != nil {
+			body, _ := json.Marshal(err)
+			writer.WriteHeader(err.StatusCode)
+			writer.Write(body)
+			return
+		}
+		body, _ := json.Marshal(result)
+		writer.Write(body)
+		writer.WriteHeader(http.StatusOK)
+	}).Methods(http.MethodDelete)
+
+	queue.Enqueue("1")
+	queue.Enqueue("2")
+	queue.Enqueue("3")
+
+	req, err := http.NewRequest(http.MethodDelete, "/queue/1", nil)
+	if err != nil {
+		t.Errorf("Error creating HTTP Request. Error: [%v]", err)
+	}
+
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, req)
+
+	var response string
+	assert.NoError(t, json.Unmarshal(recorder.Body.Bytes(), &response))
+	assert.Equal(t, http.StatusOK, recorder.Code)
+
+	items, _ := queue.Get(nil, nil)
+	assert.Len(t, items, 2)
+	assert.NotContains(t, items, response)
 }
