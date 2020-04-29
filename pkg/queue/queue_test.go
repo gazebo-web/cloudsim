fe3d6b148ea4a1772f4e7041d77ce7ac9ad2fcee
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-04-29 09:19:42 -0300

    Add integration test for move to back and count elements on the queue

--- pkg/queue/queue_test.go
+++ pkg/queue/queue_test.go
@@ -127,15 +127,126 @@ func TestIntegration_MoveToFront(t *testing.T) {
 
 	casted, ok := items[0].(string)
 	assert.True(t, ok)
-
-	assert.Equal(t, "3", casted)
-
+	assert.Equal(t, response, casted)
 }
 
 func TestIntegration_MoveToBack(t *testing.T) {
+	userService := users.NewUserServiceMock()
+
+	adminUsername := "root"
+	admin := fuel.User{
+		Name:             tools.Sptr("Admin Root"),
+		Username:         &adminUsername,
+		Email:            tools.Sptr("root@admin.com"),
+	}
+
+	userService.GetUserFromUsernameMock = func(username string) (user *fuel.User, msg *ign.ErrMsg) {
+		return &admin, nil
+	}
+
+	userService.IsSystemAdminMock = func(user string) bool {
+		return user == adminUsername
+	}
+
+	queue := NewQueue()
+	service := NewService(queue, userService)
+	controller := NewController(service)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/queue/{groupID}/back", func(writer http.ResponseWriter, request *http.Request) {
+		result, err := controller.MoveToBack(&admin, writer, request)
+		if err != nil {
+			body, _ := json.Marshal(err)
+			writer.WriteHeader(err.StatusCode)
+			writer.Write(body)
+			return
+		}
+		body, _ := json.Marshal(result)
+		writer.Write(body)
+		writer.WriteHeader(http.StatusOK)
+	}).Methods("PATCH")
+
+	queue.Enqueue("1")
+	queue.Enqueue("2")
+	queue.Enqueue("3")
+
+	req, err := http.NewRequest(http.MethodPatch, "/queue/1/back", nil)
+	if err != nil {
+		t.Errorf("Error creating HTTP Request. Error: [%v]", err)
+	}
+
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, req)
+
+	var response string
+	assert.NoError(t, json.Unmarshal(recorder.Body.Bytes(), &response))
+	assert.Equal(t, http.StatusOK, recorder.Code)
+
+	items, _ := queue.Get(nil, nil)
+
+	casted, ok := items[2].(string)
+	assert.True(t, ok)
+	assert.Equal(t, response, casted)
 }
 
 func TestIntegration_Count(t *testing.T) {
+	userService := users.NewUserServiceMock()
+
+	adminUsername := "root"
+	admin := fuel.User{
+		Name:             tools.Sptr("Admin Root"),
+		Username:         &adminUsername,
+		Email:            tools.Sptr("root@admin.com"),
+	}
+
+	userService.GetUserFromUsernameMock = func(username string) (user *fuel.User, msg *ign.ErrMsg) {
+		return &admin, nil
+	}
+
+	userService.IsSystemAdminMock = func(user string) bool {
+		return user == adminUsername
+	}
+
+	queue := NewQueue()
+	service := NewService(queue, userService)
+	controller := NewController(service)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/queue/count", func(writer http.ResponseWriter, request *http.Request) {
+		result, err := controller.Count(&admin, writer, request)
+		if err != nil {
+			body, _ := json.Marshal(err)
+			writer.WriteHeader(err.StatusCode)
+			writer.Write(body)
+			return
+		}
+		body, _ := json.Marshal(result)
+		writer.Write(body)
+		writer.WriteHeader(http.StatusOK)
+	}).Methods("PATCH")
+
+	queue.Enqueue("1")
+	queue.Enqueue("2")
+	queue.Enqueue("3")
+
+	req, err := http.NewRequest(http.MethodPatch, "/queue/count", nil)
+	if err != nil {
+		t.Errorf("Error creating HTTP Request. Error: [%v]", err)
+	}
+
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, req)
+
+	var response int
+	assert.NoError(t, json.Unmarshal(recorder.Body.Bytes(), &response))
+	assert.Equal(t, http.StatusOK, recorder.Code)
+
+	count := queue.Count()
+	assert.Equal(t, count, response)
 }
 
 func TestIntegration_Remove(t *testing.T) {
