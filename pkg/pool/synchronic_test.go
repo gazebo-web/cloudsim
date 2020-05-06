55426694ddfeea42f86b6e70da5adaf3535c3e43
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-05-06 15:45:50 -0300

    Add synchronic pool server testing function

--- pkg/pool/synchronic_test.go
+++ pkg/pool/synchronic_test.go
@@ -1,1 +1,21 @@
 package pool
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSynchronic_Serve(t *testing.T) {
+	flag := false
+
+	synchronicPool, err := NewSynchronicPool(1, func(i interface{}) {
+		value, ok := i.(bool)
+		assert.True(t, ok)
+		flag = value
+	})
+	assert.NoError(t, err)
+	synchronicPool.Serve(!flag)
+	assert.Equal(t, true, flag)
+	synchronicPool.Serve(!flag)
+	assert.Equal(t, false, flag)
+}
