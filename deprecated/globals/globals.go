054f009d2f283ebb62301b89df7871f0ef501aa0
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-05-07 11:48:44 -0300

    Add deprecated to globals on deprecated package

--- deprecated/globals/globals.go
+++ deprecated/globals/globals.go
@@ -20,65 +20,65 @@ import (
 /////////////////////////////////////////////////
 /// Define global variables here
 
-// Server encapsulates database, router, and auth0
+// Deprecated: Server encapsulates database, router, and auth0
 var Server *ign.Server
 
-// APIVersion is route api version.
+// Deprecated: APIVersion is route api version.
 // See also routes and routers
 // \todo: Add support for multiple versions.
 var APIVersion = "1.0"
 
-// Validate references the global structs validator.
+// Deprecated: Validate references the global structs validator.
 // See https://github.com/go-playground/validator.
 // We use a single instance of validator, as it caches struct info
 var Validate *validator.Validate
 
-// FormDecoder holds a reference to the global Form Decoder.
+// Deprecated: FormDecoder holds a reference to the global Form Decoder.
 // See https://github.com/go-playground/form.
 // We use a single instance of Decoder, as it caches struct info
 var FormDecoder *form.Decoder
 
-// IgnTransport holds a reference to a ign_transport node.
+// Deprecated: IgnTransport holds a reference to a ign_transport node.
 var IgnTransport *igntran.GoIgnTransportNode
 
-// IgnTransportTopic is the name of the topic to publish to (for testing purposes)
+// Deprecated: IgnTransportTopic is the name of the topic to publish to (for testing purposes)
 var IgnTransportTopic string
 
-// DefaultEmailRecipients is the default recipient when sending emails.
+// Deprecated: DefaultEmailRecipients is the default recipient when sending emails.
 // It is set using IGN_DEFAULT_EMAIL_RECIPIENT env var.
 var DefaultEmailRecipients []string
 
-// DefaultEmailSender is the default sender to use when sending emails.
+// Deprecated: DefaultEmailSender is the default sender to use when sending emails.
 // It is set using IGN_DEFAULT_EMAIL_SENDER env var.
 var DefaultEmailSender string
 
-// DisableSummaryEmails defines if cloudsim should send summary emails
+// Deprecated: DisableSummaryEmails defines if cloudsim should send summary emails
 // TODO This should probably be placed in the service configuration
 var DisableSummaryEmails = false
 
-// DisableScoreGeneration defines if cloud should generate score for simulations
+// Deprecated: DisableScoreGeneration defines if cloud should generate score for simulations
 // TODO This should probably be placed in the service configuration
 var DisableScoreGeneration = false
 
-// Service holds a reference to the Service. A proxy to ign-fuel's Users library
+// Deprecated: Service holds a reference to the Service. A proxy to ign-fuel's Users library
 // Dev note: code should not use this from globals. Instead configure your logic with arguments
 // in the constructors.
 var UserAccessor useracc.UserAccessor
 
-// Permissions manages permissions for users, roles and resources.
+// Deprecated: Permissions manages permissions for users, roles and resources.
 var Permissions *permissions.Permissions
 
-// KClientset holds a reference to the kubernetes clientset.
+// Deprecated: KClientset holds a reference to the kubernetes clientset.
 // Dev note: code should not use this from globals. Instead configure your logic with arguments
 // in the constructors. This is here to use from tests.
 var KClientset kubernetes.Interface
 
-// S3Svc holds a reference to the AWS S3 client.
+// Deprecated: S3Svc holds a reference to the AWS S3 client.
 // Dev note: code should not use this from globals. Instead configure your logic with arguments
 // in the constructors. This is here to use from tests.
 var S3Svc s3iface.S3API
 
-// EC2Svc holds a reference to the AWS EC2 client.
+// Deprecated: EC2Svc holds a reference to the AWS EC2 client.
 // Dev note: code should not use this from globals. Instead configure your logic with arguments
 // in the constructors. This is here to use from tests.
 var EC2Svc ec2iface.EC2API
