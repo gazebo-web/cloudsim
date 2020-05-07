cc841ed3a368821df80c9ef81958c889f3c96110
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-05-07 09:18:26 -0300

    Update Launch and Terminate dtos

--- pkg/workers/dtos.go
+++ pkg/workers/dtos.go
@@ -1,13 +1,15 @@
 package workers
 
+import "gitlab.com/ignitionrobotics/web/cloudsim/pkg/actions"
+
 // LaunchInput
 type LaunchInput struct {
 	GroupID string
-	Action interface{}
+	Action actions.Action
 }
 
 // TerminateInput
 type TerminateInput struct {
 	GroupID string
-	Action interface{}
+	Action actions.Action
 }
