d7455efb9ea125c3d752cf60fdb9266c58c65892
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-05-08 14:28:18 -0300

    Change actions for actions pointer for worker's input dto

--- pkg/workers/dtos.go
+++ pkg/workers/dtos.go
@@ -5,11 +5,11 @@ import "gitlab.com/ignitionrobotics/web/cloudsim/pkg/actions"
 // LaunchInput is the input needed to launch a simulation.
 type LaunchInput struct {
 	GroupID string
-	Action actions.Action
+	Action  *actions.Action
 }
 
 // TerminateInput is the input needed to terminate a simulation.
 type TerminateInput struct {
 	GroupID string
-	Action actions.Action
+	Action  *actions.Action
 }
