ed046c5bbf7ee8e37f7d8ca5d6448e463ff7bbee
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-05-08 16:47:25 -0300

    Remove circuit whitelist

--- internal/subt/circuits/circuit_repository.go
+++ internal/subt/circuits/circuit_repository.go
@@ -1,7 +1,6 @@
 package circuits
 
 import (
-	"errors"
 	"github.com/jinzhu/gorm"
 )
 
@@ -11,26 +10,16 @@ type IRepository interface {
 
 type Repository struct {
 	Db        *gorm.DB
-	whitelist map[string]bool
 }
 
 func NewRepository(db *gorm.DB) IRepository {
 	var r IRepository
 	r = &Repository{
 		Db:        db,
-		whitelist: generateWhitelist(),
 	}
 	return r
 }
 
 func (r *Repository) GetByName(name string) (*Circuit, error) {
 	panic("implement me")
-}
-
-func (r *Repository) GetFromWhitelist(name string) (*string, *bool, error) {
-	allowed, ok := r.whitelist[name]
-	if !ok {
-		return nil, nil, errors.New("circuit doesn't exist")
-	}
-	return &name, &allowed, nil
 }
