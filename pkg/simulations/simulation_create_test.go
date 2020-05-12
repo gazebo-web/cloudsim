89c42ac461c90803c10653a3eca6aedaa33fe0a8
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-05-12 17:49:45 -0300

    Add admin case for creating simulations

--- pkg/simulations/simulation_create_test.go
+++ pkg/simulations/simulation_create_test.go
@@ -33,8 +33,8 @@ type simulationTestSuite struct {
 	db *gorm.DB
 
 	userService   *users.ServiceMock
-	adminUsername string
 	admin         fuel.User
+	user         fuel.User
 
 	repository Repository
 	service    Service
@@ -79,11 +79,18 @@ func (suite *simulationTestSuite) SetupSuite() {
 	suite.router = mux.NewRouter()
 	suite.recorder = httptest.NewRecorder()
 
-	suite.adminUsername = "root"
+	adminUsername := "root"
 	suite.admin = fuel.User{
 		Name:     tools.Sptr("Admin Root"),
-		Username: &suite.adminUsername,
+		Username: &adminUsername,
 		Email:    tools.Sptr("root@admin.com"),
+	}
+
+	userUsername := "test"
+	suite.user = fuel.User{
+		Name:     tools.Sptr("User Test"),
+		Username: &userUsername,
+		Email:    tools.Sptr("test@test.com"),
 	}
 }
 
@@ -96,7 +103,66 @@ func (suite *simulationTestSuite) AfterTest(suiteName, testName string) {
 }
 
 func (suite *simulationTestSuite) TestCreate() {
+	suite.router.HandleFunc("/simulation", func(writer http.ResponseWriter, request *http.Request) {
+		result, err := suite.controller.Start(&suite.user, writer, request)
+		if err != nil {
+			body, _ := json.Marshal(err)
+			writer.WriteHeader(err.StatusCode)
+			writer.Write(body)
+			return
+		}
+		body, _ := json.Marshal(result)
+		writer.Write(body)
+		writer.WriteHeader(http.StatusOK)
+	}).Methods(http.MethodPost)
 
+	createForm := map[string]string{
+		"name":        "test1234",
+		"circuit":		"test",
+		"owner":       *suite.user.Username,
+		"robot_name":  "X1",
+		"robot_type":  "X1_SENSOR_CONFIG_1",
+		"robot_image": "infrastructureascode/aws-cli:latest",
+	}
+
+	body := bytes.NewBuffer(nil)
+	formWriter := multipart.NewWriter(body)
+
+	for key, value := range createForm {
+		suite.NoError(formWriter.WriteField(key, value))
+	}
+	suite.NoError(formWriter.Close())
+
+	var em *ign.ErrMsg
+	returnedUUID := "aaaa-bbbb-cccc-dddd"
+	suite.uuid.On("Generate").Return(returnedUUID)
+	suite.userService.On("GetUserFromUsername", *suite.user.Username).Return(suite.user, em)
+	suite.userService.On("IsSystemAdmin", *suite.user.Username).Return(false)
+	// *sim.GroupID, []per.Action{per.Read, per.Write}, *sim.Owner, *createdSim.Application
+
+	suite.userService.On("AddResourcePermission", *suite.user.Username, returnedUUID, per.Read).Return(true, em)
+	suite.userService.On("AddResourcePermission", *suite.user.Username, returnedUUID, per.Write).Return(true, em)
+	suite.userService.On("AddResourcePermission", "app_test", returnedUUID, per.Read).Return(true, em)
+	suite.userService.On("AddResourcePermission", "app_test", returnedUUID, per.Write).Return(true, em)
+
+	suite.userService.On("VerifyOwner", *suite.user.Username, *suite.user.Username, per.Read).Return(true, em)
+
+	req, err := http.NewRequest(http.MethodPost, "/simulation", body)
+	if err != nil {
+		suite.Errorf(err, "Error creating HTTP Request.")
+	}
+	req.Header.Set("Content-Type", formWriter.FormDataContentType())
+
+	suite.router.ServeHTTP(suite.recorder, req)
+
+	var response Simulation
+	b, err := ioutil.ReadAll(suite.recorder.Body)
+	suite.NoError(err)
+	suite.NoError(json.Unmarshal(b, &response))
+	suite.Equal(http.StatusOK, suite.recorder.Code)
+}
+
+func (suite *simulationTestSuite) TestCreateAdmin() {
 	suite.router.HandleFunc("/simulation", func(writer http.ResponseWriter, request *http.Request) {
 		result, err := suite.controller.Start(&suite.admin, writer, request)
 		if err != nil {
@@ -113,7 +179,6 @@ func (suite *simulationTestSuite) TestCreate() {
 	createForm := map[string]string{
 		"name":        "test1234",
 		"circuit":		"test",
-		"owner":       suite.adminUsername,
 		"robot_name":  "X1",
 		"robot_type":  "X1_SENSOR_CONFIG_1",
 		"robot_image": "infrastructureascode/aws-cli:latest",
@@ -132,14 +197,13 @@ func (suite *simulationTestSuite) TestCreate() {
 	suite.uuid.On("Generate").Return(returnedUUID)
 	suite.userService.On("GetUserFromUsername", *suite.admin.Username).Return(suite.admin, em)
 	suite.userService.On("IsSystemAdmin", *suite.admin.Username).Return(true)
-	// *sim.GroupID, []per.Action{per.Read, per.Write}, *sim.Owner, *createdSim.Application
 
-	suite.userService.On("AddResourcePermission", suite.adminUsername, returnedUUID, per.Read).Return(true, em)
-	suite.userService.On("AddResourcePermission", suite.adminUsername, returnedUUID, per.Write).Return(true, em)
+	suite.userService.On("AddResourcePermission", *suite.admin.Username, returnedUUID, per.Read).Return(true, em)
+	suite.userService.On("AddResourcePermission", *suite.admin.Username, returnedUUID, per.Write).Return(true, em)
 	suite.userService.On("AddResourcePermission", "app_test", returnedUUID, per.Read).Return(true, em)
 	suite.userService.On("AddResourcePermission", "app_test", returnedUUID, per.Write).Return(true, em)
 
-	suite.userService.On("VerifyOwner", suite.adminUsername, suite.adminUsername, per.Read).Return(true, em)
+	suite.userService.On("VerifyOwner", *suite.admin.Username, *suite.admin.Username, per.Read).Return(true, em)
 
 	req, err := http.NewRequest(http.MethodPost, "/simulation", body)
 	if err != nil {
