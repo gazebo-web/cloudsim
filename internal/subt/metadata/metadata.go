2cd98a7660392092df7c43efd6d6d66ffe755005
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-05-13 15:10:17 -0300

    Reduce amount of files

--- internal/subt/metadata/metadata.go
+++ internal/subt/metadata/metadata.go
@@ -3,13 +3,14 @@ package metadata
 import (
 	"encoding/json"
 	"gitlab.com/ignitionrobotics/web/cloudsim/internal/subt/robots"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulations"
 )
 
 type Metadata struct {
-	Circuit string         `json:"circuit"`
-	Robots  []robots.Robot `json:"robots,omitempty"`
-	RunIndex *int `json:"run_index,omitempty"`
-	WorldIndex *int        `json:"world_index,omitempty"`
+	Circuit    string         `json:"circuit"`
+	Robots     []robots.Robot `json:"robots,omitempty"`
+	RunIndex   *int           `json:"run_index,omitempty"`
+	WorldIndex *int           `json:"world_index,omitempty"`
 }
 
 func (m Metadata) ToJSON() (*string, error) {
@@ -20,4 +21,12 @@ func (m Metadata) ToJSON() (*string, error) {
 	}
 	*result = string(b)
 	return result, nil
+}
+
+func Read(sim *simulations.Simulation) (*Metadata, error) {
+	var extra Metadata
+	if err := json.Unmarshal([]byte(*sim.Extra), &extra); err != nil {
+		return nil, err
+	}
+	return &extra, nil
 }
