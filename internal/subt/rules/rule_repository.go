8d1710609091a017b11f6b2c4e2db1b5981b95ab
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-05-14 10:56:20 -0300

    Add repository and service implementation

--- internal/subt/rules/rule_repository.go
+++ internal/subt/rules/rule_repository.go
@@ -1,9 +1,35 @@
 package rules
 
+import "github.com/jinzhu/gorm"
+
 type Repository interface {
-	GetRuleByCircuitAndOwner(ruleType Type, circuit, owner string) (*Rule, error)
+	GetByCircuitAndOwner(ruleType Type, circuit, owner string) (*Rule, error)
 	GetRemainingSubmissions(owner, circuit string) (*int, error)
 }
 
 type repository struct {
+	Db *gorm.DB
+}
+
+// GetByCircuitAndOwner returns the rule value for a specific circuit and owner.
+func (r *repository) GetByCircuitAndOwner(ruleType Type, circuit, owner string) (*Rule, error) {
+	var rule Rule
+	err := r.Db.Model(&Rule{}).Where("rule_type = ?", ruleType).
+		Where("circuit = ? OR circuit IS NULL", circuit).
+		Where("owner = ? OR owner IS NULL", owner).
+		Order("owner DESC, circuit DESC").Limit(1).First(&rule).Error
+	if err != nil {
+		return nil, err
+	}
+	return &rule, nil
+}
+
+func (r *repository) GetRemainingSubmissions(owner, circuit string) (*int, error) {
+	panic("implement me")
+}
+
+func NewRepository(db *gorm.DB) Repository {
+	return &repository{
+		Db: db,
+	}
 }
--- internal/subt/rules/rule_service.go
+++ internal/subt/rules/rule_service.go
@@ -1,9 +1,24 @@
 package rules
 
 type Service interface {
-	GetRuleByCircuitAndOwner(ruleType Type, circuit, owner string) (*Rule, error)
+	GetByCircuitAndOwner(ruleType Type, circuit, owner string) (*Rule, error)
 	GetRemainingSubmissions(owner, circuit string) (*int, error)
 }
 
 type service struct {
+	repository Repository
+}
+
+func (s *service) GetByCircuitAndOwner(ruleType Type, circuit, owner string) (*Rule, error) {
+	return s.repository.GetByCircuitAndOwner(ruleType, circuit, owner)
+}
+
+func (s *service) GetRemainingSubmissions(owner, circuit string) (*int, error) {
+	return s.repository.GetRemainingSubmissions(owner, circuit)
+}
+
+func NewService(repository Repository) Service {
+	return &service{
+		repository: repository,
+	}
 }
