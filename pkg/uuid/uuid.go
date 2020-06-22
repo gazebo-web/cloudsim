7a0148b7dd4e9e0b6d9411e1dad36d35f15a3781
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-06-22 11:24:00 -0300

    Fix comments

--- pkg/uuid/uuid.go
+++ pkg/uuid/uuid.go
@@ -9,24 +9,31 @@ type UUID interface {
 	Generate() string
 }
 
+// NewUUID
 func NewUUID() UUID {
 	return &generator{}
 }
 
+// generator is a UUID implementation.
 type generator struct{}
 
+// Generate returns a UUID in string format.
 func (*generator) Generate() string {
 	return uuid.NewV4().String()
 }
 
+// MockUUID is a UUID implementation for testing purposes.
 type MockUUID struct {
 	*mock.Mock
 }
 
+// Generate returns a UUID in string format.
 func (m *MockUUID) Generate() string {
 	return m.Called().String(0)
 }
 
+// NewTestUUID creates a Mock to test the UUID generator.
+// It should only be used for testing.
 func NewTestUUID() *MockUUID {
 	return &MockUUID{
 		Mock: new(mock.Mock),
--- tools/summary.go
+++ tools/summary.go
@@ -2,6 +2,7 @@ package tools
 
 import "fmt"
 
+// GenerateSummaryFilename gets the summary filename for the given group id.
 func GenerateSummaryFilename(groupID string) string {
 	return fmt.Sprintf("%s-summary.json", groupID)
 }
