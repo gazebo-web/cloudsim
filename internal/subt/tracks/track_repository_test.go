7a98ad37ce4f8ef1a175dd2b57ea9b8689833d1f
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-07-15 16:42:31 -0300

    Add test cases for update and delete
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- internal/subt/tracks/track_repository_test.go
+++ internal/subt/tracks/track_repository_test.go
@@ -42,6 +42,11 @@ func (s *trackRepositoryTest) addMockData(key string) *Track {
 }
 
 func (s *trackRepositoryTest) TestCreate() {
+	var count int
+	err := s.db.Model(&Track{}).Count(&count).Error
+	s.NoError(err)
+	s.Equal(0, count)
+
 	track := Track{
 		Name:          "Name test",
 		Image:         "test.org/image",
@@ -51,10 +56,9 @@ func (s *trackRepositoryTest) TestCreate() {
 		MaxSimSeconds: 10,
 		Public:        true,
 	}
-	_, err := s.repository.Create(track)
+	_, err = s.repository.Create(track)
 	s.NoError(err)
 
-	var count int
 	err = s.db.Model(&Track{}).Count(&count).Error
 	s.NoError(err)
 	s.Equal(1, count)
@@ -93,16 +97,46 @@ func (s *trackRepositoryTest) TestUpdate() {
 	s.False(value.UpdatedAt.Equal(result.UpdatedAt))
 }
 
+func (s *trackRepositoryTest) TestUpdateNonExistent() {
+	_, err := s.repository.Update("TestPractice1", Track{
+		Name:          "test",
+		Image:         "test",
+		BridgeImage:   "test",
+		StatsTopic:    "test",
+		WarmupTopic:   "test",
+		MaxSimSeconds: 20,
+		Public:        false,
+	})
+	s.Error(err)
+}
+
+func (s *trackRepositoryTest) TestUpdateEmptyField() {
+	value := s.addMockData("Practice1")
+	value.BridgeImage = ""
+	_, err := s.repository.Update("TestPractice1", *value)
+	s.Error(err)
+}
+
 func (s *trackRepositoryTest) TestDelete() {
 	value := s.addMockData("Practice1")
+
+	var count int
+	err := s.db.Model(&Track{}).Count(&count).Error
+	s.NoError(err)
+	s.Equal(1, count)
+
 	result, err := s.repository.Delete("TestPractice1")
 	s.NoError(err)
 	s.Equal(value.ID, result.ID)
 
-	var count int
 	err = s.db.Model(&Track{}).Count(&count).Error
 	s.NoError(err)
 	s.Equal(0, count)
+}
+
+func (s *trackRepositoryTest) TestDeleteNonExistent() {
+	_, err := s.repository.Delete("TestPractice")
+	s.Error(err)
 }
 
 func (s *trackRepositoryTest) AfterTest() {
