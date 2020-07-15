523371c7310de44d73484f08e5cee75ca6d58bcf
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-07-15 10:22:02 -0300

    Add track repository interfaces definition
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

---
+++ internal/subt/tracks/track_repository.go
@@ -0,0 +1,30 @@
+package tracks
+
+// Repository groups a set of methods to perform CRUD operations in the database for a certain Track.
+type Repository interface {
+	repositoryCreate
+	repositoryRead
+	repositoryUpdate
+	repositoryDelete
+}
+
+// repositoryCreate has a method to Create a track from the database.
+type repositoryCreate interface {
+	Create(track Track) (*Track, error)
+}
+
+// repositoryRead has a method to get one or multiple tracks from the database.
+type repositoryRead interface {
+	Get(name string)
+	GetAll() ([]Track, error)
+}
+
+// repositoryUpdate has a method to update a track from the database.
+type repositoryUpdate interface {
+	Update(name string, track Track) (*Track, error)
+}
+
+// repositoryDelete has a method to delete a track from the database.
+type repositoryDelete interface {
+	Delete(name string) (*Track, error)
+}
