8555ee71607f4d388538c6bf2464f94e9812e187
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-07-15 12:46:52 -0300

    Add missing read, update and delete track interfaces

--- internal/subt/tracks/track_service.go
+++ internal/subt/tracks/track_service.go
@@ -1,11 +1,30 @@
 package tracks
 
-// Service groups a set of methods that have the business logic to perform CRUD operations for a Circuit.
+// Service groups a set of methods that have the business logic to perform CRUD operations for a Track.
 type Service interface {
 	serviceCreate
+	serviceRead
+	serviceUpdate
+	serviceDelete
 }
 
 // serviceCreate has the business logic for creating a Track.
 type serviceCreate interface {
 	Create(track CreateTrackInput) (*CreateTrackOutput, error)
+}
+
+// serviceRead has the business logic for reading one or multiple Tracks.
+type serviceRead interface {
+	Get(name string) (*Track, error)
+	GetAll() ([]Track, error)
+}
+
+// serviceUpdate has the business logic for updating a Track.
+type serviceUpdate interface {
+	Update(track UpdateTrackInput) (*UpdateTrackOutput, error)
+}
+
+// serviceDelete has the business logic for deleting a Track.
+type serviceDelete interface {
+	Delete(name string) (*Track, error)
 }
