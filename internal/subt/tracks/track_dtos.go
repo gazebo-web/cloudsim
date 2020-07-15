5583a4d536d70fa88ed09ea0fee9bf2528074d6c
Marcos Huck <marcos.h3012@gmail.com>
2020-07-15 19:16:04 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- internal/subt/tracks/track_dtos.go
+++ internal/subt/tracks/track_dtos.go
@@ -11,7 +11,8 @@ type CreateTrackInput struct {
 	WarmupTopic string `json:"warmup_topic"`
 	// Maximum number of allowed "simulation seconds" for each world. 0 means unlimited.
 	MaxSimSeconds int `json:"max_sim_seconds"`
-	// Public sets a track open for single runs.
+	// Public makes a track available for launching directly. 
+        // Tracks that are not public can only be launched as part of a Circuit.
 	// Default value: false
 	Public bool `json:"public"`
 }
