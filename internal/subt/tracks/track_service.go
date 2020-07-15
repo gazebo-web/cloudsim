a9ffcca86631297a69a55a4f7f51f4caf1184ed9
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-07-15 10:44:58 -0300

    Add documentation to track service interfaces

--- internal/subt/tracks/track_service.go
+++ internal/subt/tracks/track_service.go
@@ -1,9 +1,11 @@
 package tracks
 
+// Service groups a set of methods that have the business logic to perform CRUD operations for a Circuit.
 type Service interface {
 	serviceCreate
 }
 
+// serviceCreate has the business logic for creating a Track.
 type serviceCreate interface {
 	Create(track CreateTrackInput) (*CreateTrackOutput, error)
 }
