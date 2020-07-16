86b92a974615da26e444f64b4055e5d30f57e022
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-07-16 14:56:42 -0300

    Add track service test suite with Create test cases
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

---
+++ internal/subt/tracks/track_service_test.go
@@ -0,0 +1,57 @@
+package tracks
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/stretchr/testify/suite"
+	"gitlab.com/ignitionrobotics/web/ign-go"
+	"testing"
+)
+
+func TestTrackService(t *testing.T) {
+	suite.Run(t, new(trackServiceTestSuite))
+}
+
+type trackServiceTestSuite struct {
+	suite.Suite
+	db         *gorm.DB
+	repository Repository
+	service    Service
+}
+
+func (s *trackServiceTestSuite) SetupTest() {
+	var db *gorm.DB
+	dbConfig, err := ign.NewDatabaseConfigFromEnvVars()
+	s.NoError(err)
+	db, err = ign.InitDbWithCfg(&dbConfig)
+	s.db = db
+	s.db.DropTableIfExists(&Track{})
+	s.db.AutoMigrate(&Track{})
+	s.repository = NewRepository(s.db, ign.NewLoggerNoRollbar("track-service-test", ign.VerbosityDebug))
+	s.service = NewService(s.repository)
+}
+
+func (s *trackServiceTestSuite) TestCreate_OK() {
+	input := CreateTrackInput{
+		Name:          "Virtual Stix",
+		Image:         "dkr.ecr.us-east-1.amazonws.com/stix:latest",
+		BridgeImage:   "dkr.ecr.us-east-1.amazonws.com/stix-bridge:latest",
+		StatsTopic:    "/stats",
+		WarmupTopic:   "/warmup",
+		MaxSimSeconds: 3600,
+		Public:        true,
+	}
+	output, err := s.service.Create(input)
+	s.NoError(err)
+	s.Equal(input.Name, output.Name)
+	s.Equal(input.Image, output.Image)
+	s.Equal(input.BridgeImage, output.BridgeImage)
+	s.Equal(input.StatsTopic, output.StatsTopic)
+	s.Equal(input.MaxSimSeconds, output.MaxSimSeconds)
+	s.Equal(input.Public, output.Public)
+}
+
+func (s *trackServiceTestSuite) TestCreate_EmptyFields() {
+	input := CreateTrackInput{}
+	_, err := s.service.Create(input)
+	s.Error(err)
+}
