740ae919020372ed5bcc2afa6f92a222e6f477da
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-07-17 14:27:21 -0300

    Fix validate tag

--- internal/subt/tracks/track_dtos.go
+++ internal/subt/tracks/track_dtos.go
@@ -3,31 +3,31 @@ package tracks
 // CreateTrackInput is an input for creating a new track.
 type CreateTrackInput struct {
 	Name        string `json:"name" validate:"required,gt=10"`
-	Image       string `json:"image" validate="required,fqdn"`
-	BridgeImage string `json:"bridge_image" validate="required,fqdn"`
+	Image       string `json:"image" validate:"required,fqdn"`
+	BridgeImage string `json:"bridge_image" validate:"required,fqdn"`
 	// Topic used to track general stats of the simulation (runtime, sim runtime, etc.)
-	StatsTopic string `json:"stats_topic" validate="required"`
+	StatsTopic string `json:"stats_topic" validate:"required"`
 	// Topic used to track when the simulation officially starts and ends
-	WarmupTopic string `json:"warmup_topic" validate="required"`
+	WarmupTopic string `json:"warmup_topic" validate:"required"`
 	// Maximum number of allowed "simulation seconds" for each world. 0 means unlimited.
-	MaxSimSeconds int `json:"max_sim_seconds" validate="required"`
+	MaxSimSeconds int `json:"max_sim_seconds" validate:"required"`
 	// Public makes a track available for launching directly.
 	// Tracks that are not public can only be launched as part of a Circuit.
-	Public bool `json:"public" validate="required"`
+	Public bool `json:"public" validate:"required"`
 }
 
 // UpdateTrackInput is an input for updating an existent track.
 type UpdateTrackInput struct {
 	Name        string `json:"name" validate:"required,gt=10"`
-	Image       string `json:"image" validate="required,fqdn"`
-	BridgeImage string `json:"bridge_image" validate="required,fqdn"`
+	Image       string `json:"image" validate:"required,fqdn"`
+	BridgeImage string `json:"bridge_image" validate:"required,fqdn"`
 	// Topic used to track general stats of the simulation (runtime, sim runtime, etc.)
-	StatsTopic string `json:"stats_topic" validate="required"`
+	StatsTopic string `json:"stats_topic" validate:"required"`
 	// Topic used to track when the simulation officially starts and ends
-	WarmupTopic string `json:"warmup_topic" validate="required"`
+	WarmupTopic string `json:"warmup_topic" validate:"required"`
 	// Maximum number of allowed "simulation seconds" for each world. 0 means unlimited.
-	MaxSimSeconds int `json:"max_sim_seconds" validate="required"`
+	MaxSimSeconds int `json:"max_sim_seconds" validate:"required"`
 	// Public makes a track available for launching directly.
 	// Tracks that are not public can only be launched as part of a Circuit.
-	Public bool `json:"public" validate="required"`
+	Public bool `json:"public" validate:"required"`
 }
