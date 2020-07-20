6349e4a382551f7b171272f852da94a6c85dabd3
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-07-20 13:46:15 -0300

    Make repository interface use entity and filter
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- internal/pkg/repositories/repository.go
+++ internal/pkg/repositories/repository.go
@@ -1,9 +1,17 @@
 package repositories
 
+import (
+	"gitlab.com/ignitionrobotics/web/cloudsim/internal/pkg/domain"
+)
+
 // Repository represents a generic repository layer interface.
 type Repository interface {
-	GetAll(offset, limit *int) ([]interface{}, error)
-	Get(uuids []string) ([]interface{}, error)
-	Update(uuids []string, in interface{}) ([]interface{}, error)
-	Delete(uuids []string) ([]interface{}, error)
+	Create([]domain.Entity) ([]domain.Entity, error)
+	Find(offset, limit *int, filters ...Filter) ([]domain.Entity, error)
+	FindOne(filters ...Filter) (domain.Entity, error)
+	Update(data domain.Entity, filters ...Filter) error
+	Delete(filters ...Filter) error
+	SingularName() string
+	PluralName() string
+	Model() domain.Entity
 }
