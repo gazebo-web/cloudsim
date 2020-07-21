9701311e7f6df1822ee25b5d1fbc77b7e47558ac
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-07-21 17:23:25 -0300

    Group methods on entity and repository interfaces

--- internal/pkg/domain/entity.go
+++ internal/pkg/domain/entity.go
@@ -2,11 +2,6 @@ package domain
 
 // Entity represents a generic entity.
 type Entity interface {
-	Namer
-}
-
-// Namer has methods to name an entity.
-type Namer interface {
 	// TableName returns the table/collection name for a certain entity.
 	TableName() string
 	// SingularName returns the entity's name in singular.
