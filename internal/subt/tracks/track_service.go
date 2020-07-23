d9f829d8dc18c3a307a2174e0a68a101e9fc008e
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-07-23 10:18:41 -0300

    Merge all interfaces into the Service interface.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- internal/subt/tracks/track_service.go
+++ internal/subt/tracks/track_service.go
@@ -8,30 +8,10 @@ import (
 
 // Service groups a set of methods that have the business logic to perform CRUD operations for a Track.
 type Service interface {
-	serviceCreate
-	serviceRead
-	serviceUpdate
-	serviceDelete
-}
-
-// serviceCreate has the business logic for creating a Track.
-type serviceCreate interface {
 	Create(input CreateTrackInput) (*Track, error)
-}
-
-// serviceRead has the business logic for reading one or multiple Tracks.
-type serviceRead interface {
 	Get(name string) (*Track, error)
 	GetAll() ([]Track, error)
-}
-
-// serviceUpdate has the business logic for updating a Track.
-type serviceUpdate interface {
 	Update(name string, input UpdateTrackInput) (*Track, error)
-}
-
-// serviceDelete has the business logic for deleting a Track.
-type serviceDelete interface {
 	Delete(name string) (*Track, error)
 }
 
