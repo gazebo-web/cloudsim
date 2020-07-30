138976beb6e489a62355595cc1777edd9611355f
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-07-30 15:44:07 -0300

    Add documentation to s3 implementation
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/cloud/aws/s3/s3.go
+++ pkg/cloud/aws/s3/s3.go
@@ -6,18 +6,22 @@ import (
 	"time"
 )
 
+// storage is a cloud.Storage implementation.
 type storage struct {
 	API s3iface.S3API
 }
 
+// Upload uploads a file to the cloud storage.
 func (s storage) Upload(input cloud.UploadInput) error {
 	panic("implement me")
 }
 
-func (s storage) GetURL(bucket string, key string, expireIn time.Duration) string {
+// GetURL returns an URL to access the given bucket with the given key.
+func (s storage) GetURL(bucket string, key string, expiresIn time.Duration) string {
 	panic("implement me")
 }
 
+// NewStorage initializes a new cloud.Storage implementation using s3.
 func NewStorage(api s3iface.S3API) cloud.Storage {
 	return &storage{
 		API: api,
