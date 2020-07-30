c6885c12026092d2cc63b8daa5884d272e5d61d7
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-07-30 12:48:03 -0300

    Split cloud_provider.go into machines.go and storage.go
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

---
+++ pkg/cloud/storage.go
@@ -0,0 +1,22 @@
+package cloud
+
+import (
+	"bytes"
+	"time"
+)
+
+// UploadInput is the input for the Storage.Upload operation.
+// It will be used to upload a file to a certain bucket.
+type UploadInput struct {
+	Bucket        string
+	Key           string
+	File          *bytes.Reader
+	ContentLength int64
+	ContentType   string
+}
+
+// Storage groups a set of methods to interact with a Cloud Storage.
+type Storage interface {
+	Upload(input UploadInput) error
+	GetURL(bucket string, key string, expireIn time.Duration) string
+}
