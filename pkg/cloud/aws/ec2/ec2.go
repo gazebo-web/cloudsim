ddcce47d8cf4e9bb7ace66d930c58b48b0a791c8
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-07-31 12:48:48 -0300

    Add documentation to EC2 implementation
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/cloud/aws/ec2/ec2.go
+++ pkg/cloud/aws/ec2/ec2.go
@@ -5,22 +5,27 @@ import (
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/cloud"
 )
 
+// machines is a cloud.Machines implementation.
 type machines struct {
 	API ec2iface.EC2API
 }
 
+// Create creates EC2 machines.
 func (e machines) Create(input cloud.CreateMachinesInput) error {
 	panic("implement me")
 }
 
+// Terminate terminates EC2 machines.
 func (e machines) Terminate(input cloud.TerminateMachinesInput) error {
 	panic("implement me")
 }
 
+// Count counts EC2 machines.
 func (e machines) Count(input cloud.CountMachinesInput) int {
 	panic("implement me")
 }
 
+// NewMachines initializes a new cloud.Machines implementation using EC2.
 func NewMachines(api ec2iface.EC2API) cloud.Machines {
 	return &machines{
 		API: api,
