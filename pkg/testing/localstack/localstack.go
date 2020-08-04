9759e495757fa96ec0b68eb6c3bc7d26ea58a38c
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-08-04 09:41:06 -0300

    Add test case to check type returned by New.
    Add documentation to base file.
    
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/testing/localstack/localstack.go
+++ pkg/testing/localstack/localstack.go
@@ -1,13 +1,16 @@
 package localstack
 
+// LocalStack groups a set of methods to wrap LocalStack tool for testing purposes.
 type LocalStack interface {
 }
 
+// localStack is a LocalStack implementation.
 type localStack struct {
 	host     string
 	edgePort int
 }
 
+// New initializes a new LocalStack implementation.
 func New() LocalStack {
 	return &localStack{}
 }
--- pkg/testing/localstack/localstack_test.go
+++ pkg/testing/localstack/localstack_test.go
@@ -9,3 +9,9 @@ func TestNew_NotNil(t *testing.T) {
 	l := New()
 	assert.NotNil(t, l)
 }
+
+func TestNew_TypeOfStruct(t *testing.T) {
+	l := New()
+	_, ok := l.(*localStack)
+	assert.True(t, ok)
+}
