9d4490dc0ebeac8e32c343d8bcfb32615d59061d
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-08-11 10:44:47 -0300

    Split definitions in multiple files.
    Add resource interface.
    Implement resource interface in the different methods.
    
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/orchestrator/orchestrator.go
+++ pkg/orchestrator/orchestrator.go
@@ -1,7 +1,6 @@
 package orchestrator
 
 import (
-	"bytes"
 	"time"
 )
 
@@ -9,11 +8,11 @@ import (
 type Condition string
 
 var (
-	// ReadyCondition is a Condition set at ready.
+	// ReadyCondition is used to indicate that Nodes and Pods are ready.
 	ReadyCondition Condition = "Ready"
 )
 
-// Orchestrator groups a set of methods to handle Nodes and Pods.
+// Orchestrator groups a set of methods for managing a cluster.
 type Orchestrator interface {
 	Nodes() NodeManager
 	Pods() PodManager
@@ -21,54 +20,17 @@ type Orchestrator interface {
 	Ingresses() IngressManager
 }
 
-// NodeManager groups a set of methods to register nodes into a cluster.
-type NodeManager interface {
-	Waiter
+// Resource groups a set of method to identify a resource in a cluster.
+type Resource interface {
+	// Name returns the name of the resource
+	Name() string
+	// Selector returns a set of key-value pairs that identify the resource.
+	Selector() map[string]string
+	// Namespace returns the namespace where the resource lives in.
+	Namespace() string
 }
 
-// PodManager groups a set of methods to perform operation with a pod.
-type PodManager interface {
-	Exec(selector string) Executor
-	Reader(selector string) Reader
-	Waiter
-}
-
-// ServiceManager groups a set of methods to managing Services.
-// Services abstract a group of pods behind a single endpoint.
-type ServiceManager interface {
-	Get(selector, name string)
-}
-
-// IngressManager groups a set of methods to manage ingresses.
-type IngressManager interface {
-	GetByName(selector, name string)
-	Update(selector, name string, ingress interface{})
-	Rules(selector, name string) Ruler
-}
-
-// Ruler groups a set of methods to interact with an Ingress's rules.
-type Ruler interface {
-	Get(host string)
-	Upsert(host string, paths ...string)
-	Remove(host string, paths ...string)
-}
-
-// Executor groups a set of methods to execute commands or scripts inside a pod.
-type Executor interface {
-	// Cmd runs a command inside a container.
-	Cmd(command []string)
-	// Script runs a script inside a container.
-	// Could be used to run copy_to_s3.sh
-	Script(path string) error
-}
-
-// Reader groups a set of methods to read a file or the logs from a pod.
-type Reader interface {
-	File(paths ...string) (*bytes.Buffer, error)
-	Logs(lines int64) (*string, error)
-}
-
-// Waiter has a method to wait for a certain Condition to happen.
+// Waiter groups a set of methods to wait.
 type Waiter interface {
-	Wait(selector string, condition Condition, timeout time.Duration, pollFrequency time.Duration)
+	Wait(job func(), timeout time.Duration, pollFrequency time.Duration)
 }
