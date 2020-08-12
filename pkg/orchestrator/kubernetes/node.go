b5224cb547ca17eae58dd4965f88354f4cad0acc
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-08-12 11:00:25 -0300

    Add documentation to the different methods in the Node implementation.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/orchestrator/kubernetes/node.go
+++ pkg/orchestrator/kubernetes/node.go
@@ -4,28 +4,34 @@ import (
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator"
 )
 
+// Node is a Kubernetes node. It extends the generic orchestrator.Resource interface.
 type Node interface {
 	orchestrator.Resource
 }
 
+// node is a Node implementation.
 type node struct {
 	name      string
 	selector  string
 	namespace string
 }
 
+// Name returns the name of the node.
 func (n node) Name() string {
 	return n.name
 }
 
+// Selector returns the selector of the node.
 func (n node) Selector() string {
 	return n.selector
 }
 
+// Namespace returns the namespace of the node.
 func (n node) Namespace() string {
 	return n.namespace
 }
 
+// NewNode returns a new Node implementation using node.
 func NewNode(name string, namespace string, selector string) Node {
 	return &node{
 		name:      name,
