354c405cd32512e491b149fcaff8ed5b6856953b
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-08-12 10:55:01 -0300

    Add documentation to the different methods in the Kubernetes implementation.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/orchestrator/kubernetes/kubernetes.go
+++ pkg/orchestrator/kubernetes/kubernetes.go
@@ -4,27 +4,36 @@ import (
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator"
 )
 
+// k8s is an orchestrator.Orchestrator implementation.
 type k8s struct {
+	// nodeManager has a reference to a orchestrator.NodeManager implementation.
 	nodeManager orchestrator.NodeManager
-	podManager  orchestrator.PodManager
+
+	// podManager has a reference to a orchestrator.PodManager implementation.
+	podManager orchestrator.PodManager
 }
 
+// Nodes returns the Kubernetes orchestrator.NodeManager implementation.
 func (k k8s) Nodes() orchestrator.NodeManager {
 	return k.nodeManager
 }
 
+// Pods returns the Kubernetes orchestrator.PodManager implementation.
 func (k k8s) Pods() orchestrator.PodManager {
 	return k.podManager
 }
 
+// Services returns the Kubernetes orchestrator.ServiceManager implementation.
 func (k k8s) Services() orchestrator.ServiceManager {
 	panic("implement me")
 }
 
+// Ingresses returns the Kubernetes orchestrator.IngressManager implementation.
 func (k k8s) Ingresses() orchestrator.IngressManager {
 	panic("implement me")
 }
 
+// NewKubernetes returns a orchestrator.Orchestrator implementation using Kubernetes.
 func NewKubernetes(nodeManager orchestrator.NodeManager, podManager orchestrator.PodManager) orchestrator.Orchestrator {
 	return &k8s{
 		nodeManager: nodeManager,
