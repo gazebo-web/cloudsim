55854b9862a06d86e4349d2f52187d8b88576156
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-08-19 15:23:19 -0300

    Add starter test suite and incorrect status test case.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

---
+++ pkg/simulator/starter_test.go
@@ -0,0 +1,79 @@
+package simulator
+
+import (
+	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
+	"github.com/stretchr/testify/suite"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/cloud"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/cloud/aws/ec2"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/cloud/aws/s3"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator/kubernetes"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator/kubernetes/ingresses"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator/kubernetes/ingresses/rules"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator/kubernetes/nodes"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator/kubernetes/pods"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator/kubernetes/spdy"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulations"
+	simfake "gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulations/fake"
+	"gitlab.com/ignitionrobotics/web/ign-go"
+	"k8s.io/client-go/kubernetes/fake"
+	"testing"
+)
+
+func TestSimulatorStarter(t *testing.T) {
+	suite.Run(t, new(starterTestSuite))
+}
+
+type starterTestSuite struct {
+	suite.Suite
+	starter           Starter
+	k8s               *fake.Clientset
+	nodes             orchestrator.Nodes
+	spdy              *spdy.Fake
+	pods              orchestrator.Pods
+	ingresses         orchestrator.Ingresses
+	orchestrator      orchestrator.Cluster
+	ingressRules      orchestrator.IngressRules
+	logger            ign.Logger
+	awsAPI            *awsAPI
+	storage           cloud.Storage
+	machines          cloud.Machines
+	simulationService simulations.Service
+}
+
+func (s *starterTestSuite) initializeKubernetes() {
+	s.k8s = fake.NewSimpleClientset()
+	s.nodes = nodes.NewNodes(s.k8s)
+	s.spdy = spdy.NewSPDYFakeInitializer()
+	s.pods = pods.NewPods(s.k8s, s.spdy)
+	s.ingresses = ingresses.NewIngresses(s.k8s)
+	s.ingressRules = rules.NewIngressRules(s.k8s)
+	s.orchestrator = kubernetes.NewKubernetes(s.nodes, s.pods, s.ingresses, s.ingressRules)
+}
+
+func (s *starterTestSuite) initializeAWS() {
+	s.awsAPI = &awsAPI{}
+	s.storage = s3.NewStorage(s.awsAPI, s.logger)
+	s.machines = ec2.NewMachines(s.awsAPI, s.logger)
+}
+
+func (s *starterTestSuite) SetupTest() {
+	s.logger = ign.NewLoggerNoRollbar("SimulatorStarterTestSuite", ign.VerbosityDebug)
+	s.initializeKubernetes()
+	s.initializeAWS()
+	s.simulationService = simfake.NewService()
+	s.starter = NewSimulatorStarter(s.orchestrator, s.machines, s.storage, s.simulationService)
+}
+
+func (s *starterTestSuite) TestStartSimulation_IncorrectStatus() {
+	ok, err := s.starter.HasStatus("test-group-id", simulations.StatusPending)
+	s.False(ok)
+	s.Error(err)
+	s.Equal(simulations.ErrIncorrectStatus, err)
+}
+
+type awsAPI struct {
+	ec2iface.EC2API
+	s3iface.S3API
+}
