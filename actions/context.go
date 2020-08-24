7dd574e2174721ba3b18a79cf83c57c0a3f9c24a
Alejo Carballude <alejocarballude@gmail.com>
2020-08-24 10:55:20 -0400

    Improve actions.Context documentation.

--- actions/context.go
+++ actions/context.go
@@ -9,6 +9,10 @@ import (
 // Applications are free to add values to a Context through a context.Context object, but should be careful not to
 // replace values set by other actors. It is recommended to create a set of constants to use as keys, where each key is
 // composed of an application prefix followed by an identifier.
+// Note that any information that must be passed between jobs should not be passed using a context, and should be
+// returned by the job instead. This is because return values are automatically persisted, which allows an action to
+// recover after an unexpected stop (e.g. server restart), while context is lost. The context should only be used to
+// pass application-specific values used by jobs, and can be used to support a simple dependency injection scheme.
 type Context interface {
 	Ctx() ctx.Context
 }
