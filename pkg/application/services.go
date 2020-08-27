7b24387615254e01c09da0a3c431656a37989af4
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-08-27 10:16:45 -0300

    Remove store from service definition
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/application/services.go
+++ pkg/application/services.go
@@ -2,27 +2,17 @@ package application
 
 import (
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulations"
-	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/store"
 )
 
 // Services groups the services needed by an application to launch simulations.
 type Services interface {
 	// Simulations returns a service to operate over different simulations.
 	Simulations() simulations.Service
-
-	// Store returns a service to handle configuration.
-	Store() store.Store
 }
 
 // services is a Services implementation.
 type services struct {
 	simulation simulations.Service
-	store      store.Store
-}
-
-// Store returns the underlying Config Store's service.
-func (s *services) Store() store.Store {
-	return s.store
 }
 
 // Simulations returns the underlying Simulation's service.
@@ -31,9 +21,8 @@ func (s *services) Simulations() simulations.Service {
 }
 
 // NewServices initializes a new Application Services implementation.
-func NewServices(simulation simulations.Service, store store.Store) Services {
+func NewServices(simulation simulations.Service) Services {
 	return &services{
 		simulation: simulation,
-		store:      store,
 	}
 }
