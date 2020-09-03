00534000900b38fdf7d7337c6209bca406841b2d
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-09-03 17:46:43 -0300

    Add prehook to create gazebo network policy.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- internal/subt/simulator/jobs/create_gazebo_network_policy.go
+++ internal/subt/simulator/jobs/create_gazebo_network_policy.go
@@ -6,19 +6,21 @@ import (
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/actions"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulations"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulator"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulator/context"
 )
 
 // CreateGazeboServerNetworkPolicy is used to create a network policy in the orchestrator.Cluster for the gazebo server.
 var CreateGazeboServerNetworkPolicy = &actions.Job{
 	Name:            "create-gazebo-network-policy",
+	PreHooks:        []actions.JobFunc{prepareGazeboNetworkPolicyInput},
 	Execute:         createGazeboNetworkPolicy,
 	RollbackHandler: rollbackCreateGazeboNetworkPolicy,
 	InputType:       actions.GetJobDataType(simulations.GroupID("")),
 	OutputType:      actions.GetJobDataType(simulations.GroupID("")),
 }
 
-func createGazeboNetworkPolicy(ctx actions.Context, tx *gorm.DB, deployment *actions.Deployment, value interface{}) (interface{}, error) {
+func prepareGazeboNetworkPolicyInput(ctx actions.Context, tx *gorm.DB, deployment *actions.Deployment, value interface{}) (interface{}, error) {
 	gid, ok := value.(simulations.GroupID)
 	if !ok {
 		return nil, simulations.ErrInvalidGroupID
@@ -31,18 +33,84 @@ func createGazeboNetworkPolicy(ctx actions.Context, tx *gorm.DB, deployment *act
 		return nil, err
 	}
 
-	input := orchestrator.CreateNetworkPolicyInput{
-		Name:          fmt.Sprintf("%s-%s-%s", "network-policy", sim.GroupID(), "gzserver"),
-		Namespace:     simCtx.Platform().Store().Cluster().Namespace(),
-		Labels:        nil,
-		PodSelector:   nil,
-		CIDR:          simCtx.Platform().Store().Ignition().IP(),
-		WebsocketPort: 0,
-		PeersFrom:     nil,
-		PeersTo:       nil,
+	// Set up pod name
+	podName := "prefix-groupid-gzserver"
+
+	// Set up labels
+	baseLabels := map[string]string{
+		"cloudsim":          "true",
+		"pod-group":         podName,
+		"cloudsim-group-id": string(gid),
+		"SubT":              "true",
 	}
 
-	_, err = simCtx.Platform().Orchestrator().NetworkPolicies().Create(input)
+	gzServerLabels := map[string]string{
+		"cloudsim":          "true",
+		"pod-group":         podName,
+		"cloudsim-group-id": string(gid),
+		"SubT":              "true",
+		"gzserver":          "true",
+	}
+	podSelector := orchestrator.NewSelector(gzServerLabels)
+
+	bridgeLabels := map[string]string{
+		"cloudsim":          "true",
+		"pod-group":         podName,
+		"cloudsim-group-id": string(gid),
+		"SubT":              "true",
+		"comms-bridge":      "true",
+	}
+
+	cidr := fmt.Sprintf("%s/32", simCtx.Platform().Store().Ignition().IP())
+
+	input := orchestrator.CreateNetworkPolicyInput{
+		Name:        fmt.Sprintf("%s-%s-%s", "network-policy", sim.GroupID(), "gzserver"),
+		Namespace:   simCtx.Platform().Store().Cluster().Namespace(),
+		Labels:      baseLabels,
+		PodSelector: podSelector,
+		Ingresses: orchestrator.NetworkIngressRule{
+			Ports:    []int32{9002},
+			IPBlocks: []string{cidr},
+		},
+		Egresses: orchestrator.NetworkEgressRule{
+			Ports:         nil,
+			IPBlocks:      []string{cidr},
+			AllowOutbound: true,
+		},
+		PeersFrom: []orchestrator.Selector{
+			orchestrator.NewSelector(bridgeLabels),
+		},
+		PeersTo: []orchestrator.Selector{
+			orchestrator.NewSelector(bridgeLabels),
+		},
+	}
+
+	return map[string]interface{}{
+		"groupID":                  gid,
+		"createNetworkPolicyInput": input,
+	}, nil
+}
+
+func createGazeboNetworkPolicy(ctx actions.Context, tx *gorm.DB, deployment *actions.Deployment, value interface{}) (interface{}, error) {
+	// Parse input
+	inputMap, ok := value.(map[string]interface{})
+	if !ok {
+		return nil, simulator.ErrInvalidInput
+	}
+
+	createPolicyInput, ok := inputMap["createNetworkPolicyInput"].(orchestrator.CreateNetworkPolicyInput)
+	if !ok {
+		return nil, simulator.ErrInvalidInput
+	}
+
+	gid, ok := inputMap["groupID"].(simulations.GroupID)
+	if !ok {
+		return nil, simulator.ErrInvalidInput
+	}
+
+	simCtx := context.NewContext(ctx)
+
+	_, err := simCtx.Platform().Orchestrator().NetworkPolicies().Create(createPolicyInput)
 	if err != nil {
 
 		return nil, err
