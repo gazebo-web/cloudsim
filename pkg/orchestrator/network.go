22cfe2f0871c97a7962fd4c3bfcbb747a84d036e
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-09-04 16:52:47 -0300

    Add documentation to network policy dtos.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/orchestrator/network.go
+++ pkg/orchestrator/network.go
@@ -1,13 +1,21 @@
 package orchestrator
 
+// NetworkIngressRule groups a set of rules to be applied on a certain resource.
 type NetworkIngressRule struct {
-	Ports    []int32
+	// Ports are all the ports that will be opened.
+	Ports []int32
+	// IPBlocks are all the IP blocks that can access from outside.
+	// Each IPBlock should be expressed using CIDR notation.
 	IPBlocks []string
 }
 
+// NetworkEgressRule groups a set of rules to be applied on a certain resource.
 type NetworkEgressRule struct {
-	Ports         []int32
-	IPBlocks      []string
+	// Ports are all the ports that will be opened.
+	Ports []int32
+	// IPBlocks are all the IP blocks that the resource can communicate to.
+	IPBlocks []string
+	// AllowOutbound ...
 	AllowOutbound bool
 }
 
@@ -15,18 +23,25 @@ type NetworkEgressRule struct {
 type CreateNetworkPolicyInput struct {
 	// Name is the name of the network policy.
 	Name string
+
 	// Namespace is the namespace where this network policy will be created.
 	Namespace string
+
 	// Labels is the group of key-value pairs that will identify this policy.
 	Labels map[string]string
+
 	// PodSelector are the labels of the pods that this policy should match to.
 	PodSelector Selector
+
 	// PeersFrom is the group of pod selectors that are allowed to access the pods covered by this network policy.
 	PeersFrom []Selector
+
 	// PeersTo is the group of pod selectors that the pods covered by this network policy are allowed to access to.
 	PeersTo []Selector
+
 	// Ingresses groups the set of rules to apply to the ingress policy.
 	Ingresses NetworkIngressRule
+
 	// Egresses groups the set of rules to apply to the egress policy.
 	Egresses NetworkEgressRule
 }
