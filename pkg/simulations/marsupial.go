14f241e1f92ab55cc75c3313af8f6e63d17f94bb
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-09-07 12:25:55 -0300

    Adds comments to marsupial.go
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/simulations/marsupial.go
+++ pkg/simulations/marsupial.go
@@ -8,19 +8,25 @@ type Marsupial interface {
 	Child() Robot
 }
 
+// marsupial is a Marsupial implementation.
 type marsupial struct {
+	// parent has a referencere to the parent robot.
 	parent Robot
-	child  Robot
+	// child has a reference to the child robot.
+	child Robot
 }
 
+// Returns the parent robot.
 func (m marsupial) Parent() Robot {
 	return m.parent
 }
 
+// Child returns the child robot.
 func (m marsupial) Child() Robot {
 	return m.child
 }
 
+// NewMarsupial initializes a new Marsupial from the given pair of parent and child robots.
 func NewMarsupial(parent, child Robot) Marsupial {
 	return &marsupial{
 		parent: parent,
