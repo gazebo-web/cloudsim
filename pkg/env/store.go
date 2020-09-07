2035046ea8bd90d2a1801729c6d15f3d8867117b
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-09-07 18:41:35 -0300

    Add methods to return orchestrator and ignition stores.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/env/store.go
+++ pkg/env/store.go
@@ -4,17 +4,31 @@ import "gitlab.com/ignitionrobotics/web/cloudsim/pkg/store"
 
 // envStore is a store.Store implementation.
 type envStore struct {
-	machines store.Machines
+	machines     store.Machines
+	orchestrator store.Orchestrator
+	ignition     store.Ignition
+}
+
+// Orchestrator returns a store.Orchestrator implementation that reads configuration from env vars.
+func (e *envStore) Orchestrator() store.Orchestrator {
+	return e.orchestrator
+}
+
+// Ignition returns a store.Ignition implementation that reads configuration from env vars.
+func (e *envStore) Ignition() store.Ignition {
+	return e.ignition
 }
 
 // Machines returns a store.Machines implementation that reads configuration from env vars.
-func (e envStore) Machines() store.Machines {
+func (e *envStore) Machines() store.Machines {
 	return e.machines
 }
 
 // NewStore initializes a new store.Store implementation using envStore.
 func NewStore() store.Store {
 	return &envStore{
-		machines: newMachinesStore(),
+		machines:     newMachinesStore(),
+		ignition:     newIgnitionStore(),
+		orchestrator: newOrchestratorStore(),
 	}
 }
