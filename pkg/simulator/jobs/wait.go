5ace5dbbb204394954645749445a88ebccb75823
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-09-10 16:59:19 -0300

    Add generic wait job
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

---
+++ pkg/simulator/jobs/wait.go
@@ -0,0 +1,40 @@
+package jobs
+
+import (
+	"github.com/jinzhu/gorm"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/actions"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulations"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulator"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/waiter"
+	"time"
+)
+
+// WaitInput is the input used by the Wait execute function.
+type WaitInput struct {
+	GroupID       simulations.GroupID
+	Request       waiter.Waiter
+	PollFrequency time.Duration
+	Timeout       time.Duration
+}
+
+// Wait is a job that is in charge of waiting for a certain process to happen.
+var Wait = &actions.Job{
+	Execute: wait,
+}
+
+// wait is the Wait execute function. It's used to trigger the Wait method in the Request passed inside the WaitInput
+// value with the given WaitInput.Timeout and WaitInput.PollFrequency.
+// It returns an error if the request fails.
+func wait(ctx actions.Context, tx *gorm.DB, deployment *actions.Deployment, value interface{}) (interface{}, error) {
+	input, ok := value.(WaitInput)
+	if !ok {
+		return nil, simulator.ErrInvalidInput
+	}
+
+	err := input.Request.Wait(input.Timeout, input.PollFrequency)
+	if err != nil {
+		return nil, err
+	}
+
+	return input.GroupID, nil
+}
