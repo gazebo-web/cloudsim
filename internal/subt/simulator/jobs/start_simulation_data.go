7f0edbcd124476dd5cc4dfcfd777ad6545af415f
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-09-10 18:19:25 -0300

    Add start simulation data.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

---
+++ internal/subt/simulator/jobs/start_simulation_data.go
@@ -0,0 +1,12 @@
+package jobs
+
+import (
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulations"
+)
+
+// StartSimulationData contains all the information used across the start simulation action.
+type StartSimulationData struct {
+	GroupID         simulations.GroupID
+	GazeboServerPod orchestrator.Resource
+}
