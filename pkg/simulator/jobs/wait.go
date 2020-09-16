fa5f35ebf24ca29fa6f8475180dbde4a96929a43
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-09-16 09:50:40 -0300

    Refactor generic wait for instances job.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/simulator/jobs/wait.go
+++ pkg/simulator/jobs/wait.go
@@ -28,7 +28,7 @@ var Wait = &actions.Job{
 // wait is the Wait execute function. It's used to trigger the Wait method in the Request passed inside the WaitInput
 // value with the given WaitInput.Timeout and WaitInput.PollFrequency.
 // It returns an error if the request fails.
-func wait(ctx actions.Context, tx *gorm.DB, deployment *actions.Deployment, value interface{}) (interface{}, error) {
+func wait(store actions.Store, tx *gorm.DB, deployment *actions.Deployment, value interface{}) (interface{}, error) {
 	input, ok := value.(WaitInput)
 	if !ok {
 		return nil, simulator.ErrInvalidInput
