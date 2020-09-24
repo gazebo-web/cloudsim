0e9c89fa581fb30235828c36bef7173bf72f1e77
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-09-24 09:45:49 -0300

    Add prepare labels job.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- internal/subt/simulator/jobs/common.go
+++ internal/subt/simulator/jobs/common.go
@@ -2,6 +2,7 @@ package jobs
 
 import (
 	"github.com/jinzhu/gorm"
+	"gitlab.com/ignitionrobotics/web/cloudsim/internal/subt/simulator/state"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/actions"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulator/jobs"
 )
@@ -9,6 +10,19 @@ import (
 // returnState is an actions.JobFunc implementation that returns the state. It's usually used as a posthook.
 func returnState(store actions.Store, tx *gorm.DB, deployment *actions.Deployment, value interface{}) (interface{}, error) {
 	return store.State(), nil
+}
+
+// setStartState parses the input value as the StartSimulation state and sets the store with that state.
+func setStartState(store actions.Store, tx *gorm.DB, deployment *actions.Deployment, value interface{}) (interface{}, error) {
+	s := value.(*state.StartSimulation)
+	store.SetState(s)
+	return s, nil
+}
+
+// returnGroupIDFromStartState parses the input value as the StartSimulation state and returns the group id.
+func returnGroupIDFromStartState(store actions.Store, tx *gorm.DB, deployment *actions.Deployment, value interface{}) (interface{}, error) {
+	s := store.State().(*state.StartSimulation)
+	return s.GroupID, nil
 }
 
 // checkWaitError is an actions.JobFunc implementation that checks if the value returned by a job extended from the
