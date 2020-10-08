cd9fd77a6589da3ff2e27c808749997b5b06ed25
Alejo Carballude <alejocarballude@gmail.com>
2020-10-08 19:47:05 +0000

    Extract DB utils from action package

--- pkg/actions/db.go
+++ pkg/actions/db.go
@@ -1,35 +1,14 @@
 package actions
 
 import (
-	"errors"
 	"github.com/jinzhu/gorm"
+	gormUtils "gitlab.com/ignitionrobotics/web/cloudsim/pkg/utils/db/gorm"
 )
 
-// migrateModels migrates database models.
-func migrateModels(tx *gorm.DB, clean bool, models ...interface{}) error {
-	if tx != nil {
-		// Optionally clean db
-		if clean {
-			tx.DropTableIfExists(
-				models...,
-			)
-		}
-		// Create models
-		tx.AutoMigrate(
-			models...,
-		)
-	} else {
-		return errors.New("attempted to migrate with an invalid tx")
-	}
-
-	return nil
-}
-
 // MigrateDB migrates application models, indexes and keys to the database.
-func MigrateDB(tx *gorm.DB, clean bool) error {
-	return migrateModels(
+func migrateDB(tx *gorm.DB) error {
+	return gormUtils.CleanAndMigrateModels(
 		tx,
-		clean,
 		&Deployment{},
 		&deploymentData{},
 		&DeploymentError{},
