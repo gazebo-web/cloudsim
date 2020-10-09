d6d38218ab8f7a86001d4591021f3cb4fbfee078
Marcos Huck <marcos.h3012@gmail.com>
2020-10-09 20:45:53 +0000

    Add support for circuit rule deadlines.

--- simulations/subt_models.go
+++ simulations/subt_models.go
@@ -122,18 +122,21 @@ type SubTCircuitRules struct {
 	BridgeImage *string `json:"-"`
 	Worlds      *string `gorm:"size:2048" json:"-"`
 	Times       *string `json:"-"`
-	// Topic used to track general stats of the simulation (runtime, sim runtime, etc.)
+	// WorldStatsTopics is the topic used to track general stats of the simulation (runtime, sim runtime, etc.)
 	WorldStatsTopics *string `gorm:"size:2048" json:"-"`
-	// Topic used to track when the simulation officially starts and ends
+	// WorldWarmupTopics is the topic used to track when the simulation officially starts and ends
 	WorldWarmupTopics *string `gorm:"size:2048" json:"-"`
-	// Maximum number of allowed "simulation seconds" for each world. 0 means unlimited.
+	// WorldMaxSimSeconds is the maximum number of allowed "simulation seconds" for each world. 0 means unlimited.
 	WorldMaxSimSeconds *string `json:"-"`
-	// A comma separated list of seed numbers. Each seed will be used with each world run.
+	// Seeds is comma separated list of seed numbers. Each seed will be used with each world run.
 	// As an example, if field "Worlds" contains 3 worlds and "times" contains "1,2,2", then
 	// there should be 5 seeds.
-	Seeds           *string    `json:"-"`
-	MaxCredits      *int       `json:"-"`
+	Seeds      *string `json:"-"`
+	MaxCredits *int    `json:"-"`
+	// CompetitionDate is the date when all held simulations for this circuit will be launched.
 	CompetitionDate *time.Time `json:"-"`
+	// SubmissionDeadline is the deadline for all teams to submit simulations for this circuit.
+	SubmissionDeadline *time.Time `json:"-"`
 	// If this field is set to true, every team that has qualified for this circuit
 	// must be added to the table sub_t_qualified_participant.
 	// All the participants that were not added to the qualified participants table will be rejected when submitting
