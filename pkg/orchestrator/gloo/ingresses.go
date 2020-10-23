b464abeec3788a3a45797dde49da18bdc04021ce
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-10-23 15:43:00 -0300

    Rename ingresses to upstreams.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/orchestrator/gloo/ingresses.go
+++ pkg/orchestrator/gloo/ingresses.go
@@ -9,24 +9,24 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// ingresses is a gloo implementation of orchestrator.Ingresses.
-type ingresses struct {
+// upstreams is a gloo.Upstream implementation of orchestrator.Ingresses.
+type upstreams struct {
 	API     gloo.GlooV1Interface
 	Gateway gateway.GatewayV1Interface
 	Logger  ign.Logger
 }
 
 // Get returns an upstream from with a certain name in the given namespace.
-func (i *ingresses) Get(name string, namespace string) (orchestrator.Resource, error) {
-	i.Logger.Debug(fmt.Sprintf("Getting upstream with name [%s] in namespace [%s]", name, namespace))
+func (u *upstreams) Get(name string, namespace string) (orchestrator.Resource, error) {
+	u.Logger.Debug(fmt.Sprintf("Getting upstream with name [%s] in namespace [%s]", name, namespace))
 
-	out, err := i.API.Upstreams(name).Get(namespace, metav1.GetOptions{})
+	out, err := u.API.Upstreams(name).Get(namespace, metav1.GetOptions{})
 	if err != nil {
-		i.Logger.Debug(fmt.Sprintf("Getting upstream with name [%s] in namespace [%s] failed.", name, namespace))
+		u.Logger.Debug(fmt.Sprintf("Getting upstream with name [%s] in namespace [%s] failed.", name, namespace))
 		return nil, err
 	}
 
-	i.Logger.Debug(fmt.Sprintf("Getting upstream with name [%s] in namespace [%s] succeeded.", name, namespace))
+	u.Logger.Debug(fmt.Sprintf("Getting upstream with name [%s] in namespace [%s] succeeded.", name, namespace))
 
 	selector := orchestrator.NewSelector(out.Labels)
 	return orchestrator.NewResource(name, namespace, selector), nil
@@ -34,7 +34,7 @@ func (i *ingresses) Get(name string, namespace string) (orchestrator.Resource, e
 
 // NewIngresses initializes a new orchestrator.Ingresses implementation using Gloo.
 func NewIngresses(api gloo.GlooV1Interface, gw gateway.GatewayV1Interface, logger ign.Logger) orchestrator.Ingresses {
-	return &ingresses{
+	return &upstreams{
 		API:     api,
 		Gateway: gw,
 		Logger:  logger,
