1c464517a590f2c9b18a7a1b12244993a0d51376
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-10-23 15:33:00 -0300

    Add Get method implementation.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/orchestrator/gloo/ingresses.go
+++ pkg/orchestrator/gloo/ingresses.go
@@ -1,26 +1,42 @@
 package gloo
 
 import (
+	"fmt"
 	gateway "github.com/solo-io/gloo/projects/gateway/pkg/api/v1/kube/client/clientset/versioned/typed/gateway.solo.io/v1"
 	gloo "github.com/solo-io/gloo/projects/gloo/pkg/api/v1/kube/client/clientset/versioned/typed/gloo.solo.io/v1"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator"
+	"gitlab.com/ignitionrobotics/web/ign-go"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // ingresses is a gloo implementation of orchestrator.Ingresses.
 type ingresses struct {
 	API     gloo.GlooV1Interface
 	Gateway gateway.GatewayV1Interface
+	Logger  ign.Logger
 }
 
-// Get returns a Gloo Ingress from with a certain name in the given namespace.
+// Get returns an upstream from with a certain name in the given namespace.
 func (i *ingresses) Get(name string, namespace string) (orchestrator.Resource, error) {
-	panic("implement me")
+	i.Logger.Debug(fmt.Sprintf("Getting upstream with name [%s] in namespace [%s]", name, namespace))
+
+	out, err := i.API.Upstreams(name).Get(namespace, metav1.GetOptions{})
+	if err != nil {
+		i.Logger.Debug(fmt.Sprintf("Getting upstream with name [%s] in namespace [%s] failed.", name, namespace))
+		return nil, err
+	}
+
+	i.Logger.Debug(fmt.Sprintf("Getting upstream with name [%s] in namespace [%s] succeeded.", name, namespace))
+
+	selector := orchestrator.NewSelector(out.Labels)
+	return orchestrator.NewResource(name, namespace, selector), nil
 }
 
 // NewIngresses initializes a new orchestrator.Ingresses implementation using Gloo.
-func NewIngresses(api gloo.GlooV1Interface, gw gateway.GatewayV1Interface) orchestrator.Ingresses {
+func NewIngresses(api gloo.GlooV1Interface, gw gateway.GatewayV1Interface, logger ign.Logger) orchestrator.Ingresses {
 	return &ingresses{
 		API:     api,
 		Gateway: gw,
+		Logger:  logger,
 	}
 }
--- pkg/orchestrator/gloo/ingresses_test.go
+++ pkg/orchestrator/gloo/ingresses_test.go
@@ -5,6 +5,7 @@ import (
 	gloo "github.com/solo-io/gloo/projects/gloo/pkg/api/v1/kube/client/clientset/versioned/typed/gloo.solo.io/v1"
 	"github.com/stretchr/testify/assert"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator"
+	"gitlab.com/ignitionrobotics/web/ign-go"
 	"testing"
 )
 
@@ -12,8 +13,9 @@ func TestNewIngresses(t *testing.T) {
 	var api gloo.GlooV1Interface
 	var gw gateway.GatewayV1Interface
 	var ingress orchestrator.Ingresses
+	var logger ign.Logger
 
-	ingress = NewIngresses(api, gw)
+	ingress = NewIngresses(api, gw, logger)
 
 	assert.NotNil(t, ingress)
 }
