e951435dcd503a3609bb52e25c33443bf9649d9d
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-10-26 11:58:13 -0300

    Add missing file from previous commit.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/orchestrator/gloo/virtual_service_test.go
+++ pkg/orchestrator/gloo/virtual_service_test.go
@@ -2,20 +2,20 @@ package gloo
 
 import (
 	gateway "github.com/solo-io/gloo/projects/gateway/pkg/api/v1/kube/client/clientset/versioned/typed/gateway.solo.io/v1"
-	gloo "github.com/solo-io/gloo/projects/gloo/pkg/api/v1/kube/client/clientset/versioned/typed/gloo.solo.io/v1"
 	"github.com/stretchr/testify/assert"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator"
 	"gitlab.com/ignitionrobotics/web/ign-go"
 	"testing"
 )
 
-func TestNewIngresses(t *testing.T) {
-	var api gloo.GlooV1Interface
+func TestNewVirtualService(t *testing.T) {
+	var vss orchestrator.Ingresses
+
 	var gw gateway.GatewayV1Interface
-	var ingress orchestrator.Ingresses
 	var logger ign.Logger
 
-	ingress = NewIngresses(api, gw, logger)
+	vss = NewVirtualServices(gw, logger)
 
-	assert.NotNil(t, ingress)
+	assert.NotNil(t, vss)
+	assert.IsType(t, &virtualServices{}, vss)
 }
