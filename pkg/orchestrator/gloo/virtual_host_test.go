043b395d5e835aa8267a8b2be48b2f965f4ed9ef
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-10-28 15:11:40 -0300

    Fix tests.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/orchestrator/gloo/virtual_host_test.go
+++ pkg/orchestrator/gloo/virtual_host_test.go
@@ -5,7 +5,6 @@ import (
 	gatewayv1 "github.com/solo-io/gloo/projects/gateway/pkg/api/v1/kube/apis/gateway.solo.io/v1"
 	gateway "github.com/solo-io/gloo/projects/gateway/pkg/api/v1/kube/client/clientset/versioned/typed/gateway.solo.io/v1"
 	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1/core/matchers"
-	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
 	"github.com/stretchr/testify/assert"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator"
 	"gitlab.com/ignitionrobotics/web/ign-go"
@@ -20,32 +19,32 @@ func TestNewVirtualHosts(t *testing.T) {
 	assert.IsType(t, &virtualHosts{}, vhs)
 }
 
-func newTestVirtualService() *gatewayv1.VirtualService {
+func TestVirtualHosts_Get(t *testing.T) {
+
+}
+
+func TestVirtualHosts_Upsert(t *testing.T) {
+
+}
+
+func TestVirtualHost_Remove(t *testing.T) {
+
+}
+
+func newTestVirtualService(name, namespace, upstream, regex string, domains []string) *gatewayv1.VirtualService {
 	return &gatewayv1.VirtualService{Spec: v1.VirtualService{
 		VirtualHost: &v1.VirtualHost{
-			Domains: []string{"openrobotics.org"},
+			Domains: domains,
 			Routes: []*v1.Route{
 				{
-					Matchers:             []*matchers.Matcher{},
-					Action:               nil,
-					Options:              nil,
-					Name:                 "",
-					XXX_NoUnkeyedLiteral: struct{}{},
-					XXX_unrecognized:     nil,
-					XXX_sizecache:        0,
+					Matchers: []*matchers.Matcher{
+						generateMatcher(regex),
+					},
+					Action: generateRouteAction(namespace, upstream),
+					Name:   name,
 				},
 			},
-			Options:              nil,
-			XXX_NoUnkeyedLiteral: struct{}{},
-			XXX_unrecognized:     nil,
-			XXX_sizecache:        0,
 		},
-		SslConfig:            nil,
-		DisplayName:          "test",
-		Status:               core.Status{},
-		Metadata:             core.Metadata{},
-		XXX_NoUnkeyedLiteral: struct{}{},
-		XXX_unrecognized:     nil,
-		XXX_sizecache:        0,
+		DisplayName: name,
 	}}
 }
