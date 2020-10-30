82cd412027652dc13d4bdd0392486ddda56943a6
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-10-30 16:18:55 -0300

    Add missing fields to fake simulation.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- internal/subt/simulations/fake/simulation.go
+++ internal/subt/simulations/fake/simulation.go
@@ -6,30 +6,51 @@ import (
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulations/fake"
 )
 
-// SimulationConfig is used to configure a fake subt fakeSimulation.
+// SimulationConfig is used to configure a fake subt simulation.
 type SimulationConfig struct {
-	GroupID simulations.GroupID
-	Status  simulations.Status
-	Kind    simulations.Kind
-	Error   *simulations.Error
-	Image   string
-	Track   string
+	GroupID    simulations.GroupID
+	Status     simulations.Status
+	Kind       simulations.Kind
+	Error      *simulations.Error
+	Image      string
+	Track      string
+	Token      *string
+	Robots     []simulations.Robot
+	Marsupials []simulations.Robot
 }
 
-// fakeSimulation is a fake imulation implementation.
-type fakeSimulation struct {
+// simulation is a fake simulation implementation.
+type simulation struct {
 	simulations.Simulation
-	track string
+	track      string
+	token      *string
+	robots     []simulations.Robot
+	marsupials []simulations.Marsupial
+}
+
+// Token returns the access token of a simulation.
+func (s *simulation) Token() *string {
+	return s.token
+}
+
+// Robots return the list of simulations.Robot that will run in the simulation.
+func (s *simulation) Robots() []simulations.Robot {
+	return s.robots
+}
+
+// Marsupials return the list of simulations.Marsupial for the simulation.
+func (s *simulation) Marsupials() []simulations.Marsupial {
+	return s.marsupials
 }
 
 // Track returns the track of a simulation.
-func (s *fakeSimulation) Track() string {
+func (s *simulation) Track() string {
 	return s.track
 }
 
 // NewSimulation initializes a new Simulation interface using a fake implementation.
 func NewSimulation(config SimulationConfig) subt.Simulation {
-	return &fakeSimulation{
+	return &simulation{
 		Simulation: fake.NewSimulation(config.GroupID, config.Status, config.Kind, config.Error, config.Image),
 		track:      config.Track,
 	}
