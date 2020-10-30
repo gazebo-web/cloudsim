f2a94fdd3a43e5d0080bdebd6945d1709f32da23
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-10-30 15:43:49 -0300

    Fix lint error.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- internal/subt/tracks/fake/track_service.go
+++ internal/subt/tracks/fake/track_service.go
@@ -5,44 +5,44 @@ import (
 	"gitlab.com/ignitionrobotics/web/cloudsim/internal/subt/tracks"
 )
 
-// fakeService is a fake tracks.Service implementation.
-type fakeService struct {
+// Service is a fake tracks.Service implementation.
+type Service struct {
 	*mock.Mock
 }
 
 // Create mocks the Create method.
-func (s *fakeService) Create(input tracks.CreateTrackInput) (*tracks.Track, error) {
+func (s *Service) Create(input tracks.CreateTrackInput) (*tracks.Track, error) {
 	args := s.Called(input)
 	return args.Get(0).(*tracks.Track), args.Error(1)
 }
 
 // Get mocks the Get method.
-func (s *fakeService) Get(name string) (*tracks.Track, error) {
+func (s *Service) Get(name string) (*tracks.Track, error) {
 	args := s.Called(name)
 	return args.Get(0).(*tracks.Track), args.Error(1)
 }
 
 // GetAll mocks the GetAll method.
-func (s *fakeService) GetAll() ([]tracks.Track, error) {
+func (s *Service) GetAll() ([]tracks.Track, error) {
 	args := s.Called()
 	return args.Get(0).([]tracks.Track), args.Error(1)
 }
 
 // Update mocks the Update method.
-func (s *fakeService) Update(name string, input tracks.UpdateTrackInput) (*tracks.Track, error) {
+func (s *Service) Update(name string, input tracks.UpdateTrackInput) (*tracks.Track, error) {
 	args := s.Called(name, input)
 	return args.Get(0).(*tracks.Track), args.Error(1)
 }
 
 // Delete mocks the Delete method.
-func (s *fakeService) Delete(name string) (*tracks.Track, error) {
+func (s *Service) Delete(name string) (*tracks.Track, error) {
 	args := s.Called(name)
 	return args.Get(0).(*tracks.Track), args.Error(1)
 }
 
 // NewService initializes a fake tracks.Service implementation.
-func NewService() *fakeService {
-	return &fakeService{
+func NewService() *Service {
+	return &Service{
 		Mock: new(mock.Mock),
 	}
 }
