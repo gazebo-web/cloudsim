e462361a79174e19b216987f6c4ce7354ff36e43
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-12-01 11:53:38 -0300

    Add launch field computer pods job.

---
+++ internal/subt/simulator/jobs/launch_field_computer_pods.go
@@ -0,0 +1,79 @@
+package jobs
+
+import (
+	"github.com/jinzhu/gorm"
+	subtapp "gitlab.com/ignitionrobotics/web/cloudsim/internal/subt/application"
+	subt "gitlab.com/ignitionrobotics/web/cloudsim/internal/subt/simulations"
+	"gitlab.com/ignitionrobotics/web/cloudsim/internal/subt/simulator/state"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/actions"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulations"
+	"time"
+)
+
+func prepareFieldComputerPodInput(store actions.Store, tx *gorm.DB, deployment *actions.Deployment, value interface{}) (interface{}, error) {
+	s := store.State().(*state.StartSimulation)
+
+	sim, err := s.Services().Simulations().Get(s.GroupID)
+	if err != nil {
+
+	}
+
+	subtSim := sim.(subt.Simulation)
+
+	var pods []orchestrator.CreatePodInput
+
+	for i, r := range subtSim.Robots() {
+		robotID := subtapp.GetRobotID(i)
+		// Create field computer input
+		pods = append(pods, prepareFieldComputerCreatePodInput(configFieldComputerPod{
+			groupID:                s.GroupID,
+			robotID:                robotID,
+			namespace:              s.Platform().Store().Orchestrator().Namespace(),
+			labels:                 nil,
+			terminationGracePeriod: s.Platform().Store().Orchestrator().TerminationGracePeriod(),
+			nodeSelector:           subtapp.GetNodeLabelsFieldComputer(s.GroupID, r),
+			containerImage:         "",
+			robotName:              r.Name(),
+			nameservers:            s.Platform().Store().Orchestrator().Nameservers(),
+		}))
+	}
+
+	return pods, nil
+}
+
+type configFieldComputerPod struct {
+	groupID                simulations.GroupID
+	robotID                string
+	namespace              string
+	labels                 map[string]string
+	terminationGracePeriod time.Duration
+	nodeSelector           orchestrator.Selector
+	containerImage         string
+	robotName              string
+	nameservers            []string
+}
+
+func prepareFieldComputerCreatePodInput(c configFieldComputerPod) orchestrator.CreatePodInput {
+	in := configPod{
+		name:                      subtapp.GetPodNameFieldComputer(c.groupID, c.robotID),
+		namespace:                 c.namespace,
+		labels:                    c.labels,
+		restartPolicy:             orchestrator.RestartPolicyNever,
+		terminationGracePeriod:    c.terminationGracePeriod,
+		nodeSelector:              c.nodeSelector,
+		containerName:             "field-computer",
+		image:                     c.containerImage,
+		args:                      nil,
+		privileged:                false,
+		allowPrivilegesEscalation: true,
+		ports:                     nil,
+		volumes:                   nil,
+		envVars: map[string]string{
+			"ROBOT_NAME": c.robotName,
+		},
+		nameservers: nil,
+	}
+
+	return preparePod(in)
+}
