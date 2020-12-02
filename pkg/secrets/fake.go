a9c05b5334e9f8607978c1b50fa04b732df1a9f3
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-12-02 15:29:54 -0300

    Add documentation to fake secrets implementation.

--- pkg/secrets/fake.go
+++ pkg/secrets/fake.go
@@ -5,16 +5,19 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+// Fake is a fake Secrets implementation.
 type Fake struct {
 	*mock.Mock
 }
 
+// Get mocks the Secrets.Get method.
 func (f *Fake) Get(ctx context.Context, name, namespace string) (*Secret, error) {
 	args := f.Called(ctx, name, namespace)
 	s := args.Get(0).(*Secret)
 	return s, args.Error(1)
 }
 
+// NewFakeSecrets initializes a new fake implementation for secrets.
 func NewFakeSecrets() *Fake {
 	return &Fake{
 		Mock: new(mock.Mock),
