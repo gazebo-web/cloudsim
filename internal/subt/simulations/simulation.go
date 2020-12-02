e8df93ec59e32511df2ae24fa276952ce057774e
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-12-02 16:04:44 -0300

    Complete documentation on IsRobotChildMarsupial.

--- internal/subt/simulations/simulation.go
+++ internal/subt/simulations/simulation.go
@@ -19,7 +19,7 @@ type Simulation interface {
 	GetMarsupials() []simulations.Marsupial
 }
 
-// IsRobotChildMarsupial
+// IsRobotChildMarsupial checks if the given robot is a child from the given slice of marsupials.
 func IsRobotChildMarsupial(marsupials []simulations.Marsupial, robot simulations.Robot) bool {
 	for _, m := range marsupials {
 		if robot.IsEqual(m.Child()) {
