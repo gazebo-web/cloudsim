874c5e1189f422af7fb97d670ae502571a77c848
Marcos Huck <marcos@huck.com.ar>
2020-12-10 14:23:07 -0300

    Update job name.
    Signed-off-by: Marcos Huck <marcos@huck.com.ar>

--- internal/subt/simulator/jobs/check_sim_no_parent.go
+++ internal/subt/simulator/jobs/check_sim_no_parent.go
@@ -7,7 +7,7 @@ import (
 
 // CheckSimulationIsNotParent is a job in charge of checking if a simulation is not a parent simulation.
 var CheckSimulationIsNotParent = GenerateCheckSimulationNoKindJob(
-	"check-simulation-parent",
+	"check-simulation-no-parent",
 	simulations.SimParent,
 	&state.StartSimulation{},
 	&state.StartSimulation{},
