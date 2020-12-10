67081113ad471fd236e526cce549cb8ad31deb95
Marcos Huck <marcos@huck.com.ar>
2020-12-10 10:31:29 -0300

    Add start simulation jobs.
    Signed-off-by: Marcos Huck <marcos@huck.com.ar>

--- internal/subt/simulator/simulator_jobs.go
+++ internal/subt/simulator/simulator_jobs.go
@@ -1,11 +1,35 @@
 package simulator
 
 import (
+	"gitlab.com/ignitionrobotics/web/cloudsim/internal/subt/simulator/jobs"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/actions"
 )
 
 // JobsStartSimulation groups the jobs needed to start a simulation.
-var JobsStartSimulation = actions.Jobs{}
+var JobsStartSimulation = actions.Jobs{
+	jobs.CheckSimulationPendingStatus,
+	jobs.CheckSimulationIsParent,
+	jobs.CheckSimulationNoErrors,
+	jobs.SetSimulationStatusToLaunchInstances,
+	jobs.LaunchInstances,
+	jobs.SetSimulationStatusToWaitInstances,
+	jobs.WaitForInstances,
+	jobs.SetSimulationStatusToWaitNodes,
+	jobs.WaitForNodes,
+	jobs.SetSimulationStatusToLaunchPods,
+	// jobs.CreateGazeboServerNetworkPolicy,
+	jobs.LaunchGazeboServerPod,
+	jobs.WaitForGazeboServerPod,
+	// jobs.LaunchGazeboServerCopyPod,
+	// jobs.ConfigureGlooIngress,
+	// jobs.LaunchWebsocketService,
+	// jobs.CreateRobotNetworkPolicies,
+	// jobs.LaunchCommsBridgePods,
+	// jobs.LaunchFieldComputerPods,
+	jobs.SetSimulationStatusToWaitPods,
+	//jobs.WaitForRobotPods,
+	jobs.SetSimulationStatusToRunning,
+}
 
 // JobsStopSimulation groups the jobs needed to stop a simulation.
 var JobsStopSimulation = actions.Jobs{}
