9f80f9ca165b2eb9eb3c581e0fa974ac4dbb01e0
Marcos Huck <marcos@huck.com.ar>
2020-12-22 15:00:59 -0300

    Fix stop check kind job generator.
    Signed-off-by: Marcos Huck <marcos@huck.com.ar>

--- internal/subt/simulator/jobs/check_sim_no_parent.go
+++ internal/subt/simulator/jobs/check_sim_no_parent.go
@@ -7,16 +7,24 @@ import (
 
 // CheckStartSimulationIsNotParent is a job in charge of checking if a simulation is not a parent simulation.
 var CheckStartSimulationIsNotParent = GenerateCheckSimulationNotOfKindJob(
-	"check-start-simulation-no-parent",
-	simulations.SimParent,
-	&state.StartSimulation{},
-	&state.StartSimulation{},
+	ConfigCheckSimulationNotOfKindJob{
+		Name:               "check-start-simulation-no-parent",
+		Kind:               simulations.SimParent,
+		PreHooks:           nil,
+		PreparationPreHook: generateCheckStartSimulationNotOfKindInputPreHook(simulations.SimParent),
+		InputType:          &state.StartSimulation{},
+		OutputType:         &state.StartSimulation{},
+	},
 )
 
 // CheckStopSimulationIsNotParent is a job in charge of checking if a simulation is not a parent simulation.
 var CheckStopSimulationIsNotParent = GenerateCheckSimulationNotOfKindJob(
-	"check-stop-simulation-no-parent",
-	simulations.SimParent,
-	&state.StartSimulation{},
-	&state.StartSimulation{},
+	ConfigCheckSimulationNotOfKindJob{
+		Name:               "check-stop-simulation-no-parent",
+		Kind:               simulations.SimParent,
+		PreHooks:           nil,
+		PreparationPreHook: generateCheckStopSimulationNotOfKindInputPreHook(simulations.SimParent),
+		InputType:          &state.StopSimulation{},
+		OutputType:         &state.StopSimulation{},
+	},
 )
--- internal/subt/simulator/jobs/generator_check_kind.go
+++ internal/subt/simulator/jobs/generator_check_kind.go
@@ -8,24 +8,49 @@ import (
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulator/jobs"
 )
 
-// GenerateCheckSimulationNotOfKindJob is a job generator for checking that a certain simulation is not of a certain kind.
-func GenerateCheckSimulationNotOfKindJob(name string, kind simulations.Kind, inputType, outputType interface{}) *actions.Job {
-	createCheckKindInput := generateCheckSimulationNotOfKindInputPreHook(kind)
+type ConfigCheckSimulationNotOfKindJob struct {
+	Name               string
+	Kind               simulations.Kind
+	PreHooks           []actions.JobFunc
+	PreparationPreHook actions.JobFunc
+	InputType          interface{}
+	OutputType         interface{}
+}
 
+// GenerateCheckSimulationNotOfKindJob is a job generator for checking that a certain simulation is not of a certain kind.
+func GenerateCheckSimulationNotOfKindJob(config ConfigCheckSimulationNotOfKindJob) *actions.Job {
 	return jobs.CheckSimulationKind.Extend(actions.Job{
-		Name:       name,
-		PreHooks:   []actions.JobFunc{setStartState, createCheckKindInput},
+		Name:       config.Name,
+		PreHooks:   append(config.PreHooks, config.PreparationPreHook),
 		PostHooks:  []actions.JobFunc{assertSimulationNotOfKind, returnState},
-		InputType:  actions.GetJobDataType(inputType),
-		OutputType: actions.GetJobDataType(outputType),
+		InputType:  actions.GetJobDataType(config.InputType),
+		OutputType: actions.GetJobDataType(config.OutputType),
 	})
 }
 
-// generateCheckSimulationNotOfKindInputPreHook generates a pre-hook to get the simulation from a certain group ID
+// generateCheckStartSimulationNotOfKindInputPreHook generates a pre-hook to get the simulation from a certain group ID
 // passed in the action store and prepares the proper dto for the generic job to check simulation is not of a certain kind.
-func generateCheckSimulationNotOfKindInputPreHook(kind simulations.Kind) actions.JobFunc {
+func generateCheckStartSimulationNotOfKindInputPreHook(kind simulations.Kind) actions.JobFunc {
 	return func(store actions.Store, tx *gorm.DB, deployment *actions.Deployment, value interface{}) (interface{}, error) {
 		s := value.(*state.StartSimulation)
+
+		sim, err := s.Services().Simulations().Get(s.GroupID)
+		if err != nil {
+			return nil, err
+		}
+
+		return jobs.CheckSimulationKindInput{
+			Simulation: sim,
+			Kind:       kind,
+		}, nil
+	}
+}
+
+// generateCheckStopSimulationNotOfKindInputPreHook generates a pre-hook to get the simulation from a certain group ID
+// passed in the action store and prepares the proper dto for the generic job to check simulation is not of a certain kind.
+func generateCheckStopSimulationNotOfKindInputPreHook(kind simulations.Kind) actions.JobFunc {
+	return func(store actions.Store, tx *gorm.DB, deployment *actions.Deployment, value interface{}) (interface{}, error) {
+		s := value.(*state.StopSimulation)
 
 		sim, err := s.Services().Simulations().Get(s.GroupID)
 		if err != nil {
