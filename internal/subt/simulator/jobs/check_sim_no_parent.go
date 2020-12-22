33397c74f61fc9ba579b4985dd269e0b91ac2f15
Marcos Huck <marcos@huck.com.ar>
2020-12-22 14:33:18 -0300

    Add check not parent job.
    Signed-off-by: Marcos Huck <marcos@huck.com.ar>

--- internal/subt/simulator/jobs/check_sim_no_parent.go
+++ internal/subt/simulator/jobs/check_sim_no_parent.go
@@ -5,9 +5,17 @@ import (
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulations"
 )
 
-// CheckSimulationIsNotParent is a job in charge of checking if a simulation is not a parent simulation.
-var CheckSimulationIsNotParent = GenerateCheckSimulationNotOfKindJob(
-	"check-simulation-no-parent",
+// CheckStartSimulationIsNotParent is a job in charge of checking if a simulation is not a parent simulation.
+var CheckStartSimulationIsNotParent = GenerateCheckSimulationNotOfKindJob(
+	"check-start-simulation-no-parent",
+	simulations.SimParent,
+	&state.StartSimulation{},
+	&state.StartSimulation{},
+)
+
+// CheckStopSimulationIsNotParent is a job in charge of checking if a simulation is not a parent simulation.
+var CheckStopSimulationIsNotParent = GenerateCheckSimulationNotOfKindJob(
+	"check-stop-simulation-no-parent",
 	simulations.SimParent,
 	&state.StartSimulation{},
 	&state.StartSimulation{},
