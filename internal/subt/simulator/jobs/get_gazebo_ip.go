c497fd7c3b5d0dc201c3079ce65500e0252abca7
Marcos Huck <marcos@huck.com.ar>
2021-01-08 15:05:51 -0300

    Add documentation.
    Signed-off-by: Marcos Huck <marcos@huck.com.ar>

--- internal/subt/simulator/jobs/get_gazebo_ip.go
+++ internal/subt/simulator/jobs/get_gazebo_ip.go
@@ -7,6 +7,7 @@ import (
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/actions"
 )
 
+// GetGazeboIP is a job in charge of getting the IP from the simulation's gazebo server pod.
 var GetGazeboIP = &actions.Job{
 	Name:            "get-gzserver-pod-ip",
 	PreHooks:        []actions.JobFunc{setStartState},
@@ -17,12 +18,14 @@ var GetGazeboIP = &actions.Job{
 	OutputType:      actions.GetJobDataType(&state.StartSimulation{}),
 }
 
+// clearGazeboIP will remove the gazebo server ip if the GetGazeboIP job fails.
 func clearGazeboIP(store actions.Store, tx *gorm.DB, deployment *actions.Deployment, value interface{}, err error) (interface{}, error) {
 	s := store.State().(*state.StartSimulation)
 	s.GazeboServerIP = ""
 	return nil, nil
 }
 
+// getGazeboIP gets the gazebo server pod IP and assigns it to the start simulation state.
 func getGazeboIP(store actions.Store, tx *gorm.DB, deployment *actions.Deployment, value interface{}) (interface{}, error) {
 	s := store.State().(*state.StartSimulation)
 
