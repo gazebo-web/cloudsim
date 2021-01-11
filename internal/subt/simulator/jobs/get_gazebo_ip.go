3fc30a23e077fcdf14d7b12dd8e0eb5cdc4e417f
Marcos Huck <marcos@huck.com.ar>
2021-01-11 15:01:35 -0300

    Remove rollback handler.
    Signed-off-by: Marcos Huck <marcos@huck.com.ar>

--- internal/subt/simulator/jobs/get_gazebo_ip.go
+++ internal/subt/simulator/jobs/get_gazebo_ip.go
@@ -9,20 +9,12 @@ import (
 
 // GetGazeboIP is a job in charge of getting the IP from the simulation's gazebo server pod.
 var GetGazeboIP = &actions.Job{
-	Name:            "get-gzserver-pod-ip",
-	PreHooks:        []actions.JobFunc{setStartState},
-	Execute:         getGazeboIP,
-	PostHooks:       []actions.JobFunc{returnState},
-	RollbackHandler: clearGazeboIP,
-	InputType:       actions.GetJobDataType(&state.StartSimulation{}),
-	OutputType:      actions.GetJobDataType(&state.StartSimulation{}),
-}
-
-// clearGazeboIP will remove the gazebo server ip if the GetGazeboIP job fails.
-func clearGazeboIP(store actions.Store, tx *gorm.DB, deployment *actions.Deployment, value interface{}, err error) (interface{}, error) {
-	s := store.State().(*state.StartSimulation)
-	s.GazeboServerIP = ""
-	return nil, nil
+	Name:       "get-gzserver-pod-ip",
+	PreHooks:   []actions.JobFunc{setStartState},
+	Execute:    getGazeboIP,
+	PostHooks:  []actions.JobFunc{returnState},
+	InputType:  actions.GetJobDataType(&state.StartSimulation{}),
+	OutputType: actions.GetJobDataType(&state.StartSimulation{}),
 }
 
 // getGazeboIP gets the gazebo server pod IP and assigns it to the start simulation state.
