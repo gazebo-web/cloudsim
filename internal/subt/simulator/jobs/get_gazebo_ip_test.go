eb2d6f76830d8ce15598975b27d877564a1d19e7
Marcos Huck <marcos@huck.com.ar>
2021-01-11 15:40:31 -0300

    Add test case to GetGazeboIP test suite.
    Signed-off-by: Marcos Huck <marcos@huck.com.ar>

--- internal/subt/simulator/jobs/get_gazebo_ip_test.go
+++ internal/subt/simulator/jobs/get_gazebo_ip_test.go
@@ -2,6 +2,8 @@ package jobs
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gitlab.com/ignitionrobotics/web/cloudsim/internal/subt/application"
 	"gitlab.com/ignitionrobotics/web/cloudsim/internal/subt/simulator/state"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/actions"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator/kubernetes"
@@ -9,14 +11,20 @@ import (
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator/kubernetes/spdy"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/platform"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulations"
+	sfake "gitlab.com/ignitionrobotics/web/cloudsim/pkg/store/fake"
 	"gitlab.com/ignitionrobotics/web/ign-go"
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kfake "k8s.io/client-go/kubernetes/fake"
 	"testing"
 )
 
 func TestGetGazeboServerIPFailsWhenNotSet(t *testing.T) {
 	logger := ign.NewLoggerNoRollbar("TestGetGazeboServerIPFailsWhenNotSet", ign.VerbosityDebug)
+
+	storeOrchestrator := sfake.NewFakeOrchestrator()
+	storeOrchestrator.On("Namespace").Return("default")
+	fakeStore := sfake.NewFakeStore(nil, storeOrchestrator, nil)
 
 	spdyInit := spdy.NewSPDYFakeInitializer()
 
@@ -35,6 +43,7 @@ func TestGetGazeboServerIPFailsWhenNotSet(t *testing.T) {
 
 	p := platform.NewPlatform(platform.Components{
 		Cluster: ks,
+		Store:   fakeStore,
 	})
 
 	gid := simulations.GroupID("test-12345")
@@ -46,4 +55,52 @@ func TestGetGazeboServerIPFailsWhenNotSet(t *testing.T) {
 	_, err := GetGazeboIP.Run(store, nil, nil, s)
 
 	assert.Error(t, err)
+}
+
+func TestGetGazeboIP(t *testing.T) {
+	logger := ign.NewLoggerNoRollbar("TestGetGazeboIP", ign.VerbosityDebug)
+
+	storeOrchestrator := sfake.NewFakeOrchestrator()
+	storeOrchestrator.On("Namespace").Return("default")
+	fakeStore := sfake.NewFakeStore(nil, storeOrchestrator, nil)
+
+	spdyInit := spdy.NewSPDYFakeInitializer()
+
+	ip := "127.0.0.1"
+	gid := simulations.GroupID("test-12345")
+
+	pod := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      application.GetPodNameGazeboServer(gid),
+			Namespace: "default",
+		},
+		Status: v1.PodStatus{
+			PodIP: ip,
+		},
+	}
+
+	client := kfake.NewSimpleClientset(pod)
+	po := pods.NewPods(client, spdyInit, logger)
+
+	ks := kubernetes.NewCustomKubernetes(kubernetes.Config{
+		Pods: po,
+	})
+
+	p := platform.NewPlatform(platform.Components{
+		Cluster: ks,
+		Store:   fakeStore,
+	})
+
+	s := state.NewStartSimulation(p, nil, gid)
+
+	store := actions.NewStore(s)
+
+	out, err := GetGazeboIP.Run(store, nil, nil, s)
+
+	require.NoError(t, err)
+
+	result, ok := out.(*state.StartSimulation)
+
+	assert.True(t, ok)
+	assert.Equal(t, ip, result.GazeboServerIP)
 }
