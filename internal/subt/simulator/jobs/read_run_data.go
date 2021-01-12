eaf651fa456008c3fc179bba6adf0b09c6a658cf
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-01-12 14:18:38 -0300

    Add job to read run data.
    Add run data to stop state.
    
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

---
+++ internal/subt/simulator/jobs/read_run_data.go
@@ -0,0 +1,43 @@
+package jobs
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	subtapp "gitlab.com/ignitionrobotics/web/cloudsim/internal/subt/application"
+	"gitlab.com/ignitionrobotics/web/cloudsim/internal/subt/simulator/state"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/actions"
+)
+
+// ReadRunData is a job in charge of reading the run data from a gzserver copy pod for the simulation that is being processed.
+var ReadRunData = actions.Job{
+	Name:       "read-simulation-run-data",
+	PreHooks:   []actions.JobFunc{setStopState},
+	Execute:    readRunData,
+	PostHooks:  []actions.JobFunc{returnState},
+	InputType:  actions.GetJobDataType(&state.StopSimulation{}),
+	OutputType: actions.GetJobDataType(&state.StopSimulation{}),
+}
+
+// readRunData is the main execute function for the ReadRunData job.
+func readRunData(store actions.Store, tx *gorm.DB, deployment *actions.Deployment, value interface{}) (interface{}, error) {
+	s := store.State().(*state.StopSimulation)
+
+	path := fmt.Sprintf("%s/run.yml", s.Platform().Store().Ignition().GazeboServerLogsPath())
+
+	body, err := readFileContentFromPod(
+		s.Platform().Orchestrator().Pods(),
+		subtapp.GetPodNameGazeboServerCopy(s.GroupID),
+		s.Platform().Store().Orchestrator().Namespace(),
+		path,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set run data
+	s.RunData = string(body)
+	store.SetState(s)
+
+	// Return state
+	return s, nil
+}
