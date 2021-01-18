e8ff336c4b14f9f2b6dd1a4d6cca23bf71cce47e
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-01-18 18:03:47 -0300

    Add generateSummary function implementation.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- internal/subt/simulator/jobs/generate_summary.go
+++ internal/subt/simulator/jobs/generate_summary.go
@@ -4,8 +4,10 @@ import (
 	"github.com/jinzhu/gorm"
 	"gitlab.com/ignitionrobotics/web/cloudsim/internal/subt/simulator/state"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/actions"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulations"
 )
 
+// GenerateSummary is a job in charge of generating the summary for a certain simulation.
 var GenerateSummary = &actions.Job{
 	Name:       "generate-summary",
 	PreHooks:   []actions.JobFunc{setStopState},
@@ -15,6 +17,23 @@ var GenerateSummary = &actions.Job{
 	OutputType: actions.GetJobDataType(&state.StopSimulation{}),
 }
 
+// generateSummary is the main execute function of the GenerateSummary job. It's in charge of getting all processed data in previous
+// jobs and save a data structure with the summary.
 func generateSummary(store actions.Store, tx *gorm.DB, deployment *actions.Deployment, value interface{}) (interface{}, error) {
+	s := store.State().(*state.StopSimulation)
 
+	s.Summary = simulations.Summary{
+		Score:                  s.Score,
+		SimTimeDurationAvg:     float64(s.Stats.SimulationTime),
+		SimTimeDurationStdDev:  0,
+		RealTimeDurationAvg:    float64(s.Stats.RealTime),
+		RealTimeDurationStdDev: 0,
+		ModelCountAvg:          float64(s.Stats.ModelCount),
+		ModelCountStdDev:       0,
+		Sources:                "",
+	}
+
+	store.SetState(s)
+
+	return store, nil
 }
--- internal/subt/simulator/state/stop_simulation.go
+++ internal/subt/simulator/state/stop_simulation.go
@@ -14,21 +14,7 @@ type StopSimulation struct {
 	GroupID  simulations.GroupID
 	Score    float64
 	Stats    simulations.Statistics
-}
-
-// Platform returns the underlying platform.
-func (s *StopSimulation) Platform() platform.Platform {
-	return s.platform
-}
-
-// Services returns the underlying application services.
-func (s *StopSimulation) Services() application.Services {
-	return s.services
-}
-
-// SubTServices returns the subt specific application services.
-func (s *StopSimulation) SubTServices() subtapp.Services {
-	return s.services
+	Summary  simulations.Summary
 }
 
 // Platform returns the underlying platform.
