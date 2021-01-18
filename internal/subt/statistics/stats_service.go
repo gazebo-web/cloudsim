b0dbf6f8948be795d03f4bc359b732a4b364f378
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-01-18 14:10:28 -0300

    Add missing documentation to statistics service.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- internal/subt/statistics/stats_service.go
+++ internal/subt/statistics/stats_service.go
@@ -2,7 +2,8 @@ package statistics
 
 import "gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulations"
 
+// Service groups a set of methods to handle statistics for simulations.
 type Service interface {
-	// Save saves the given score and Statistics for the given GroupID.
+	// Save saves the given score and Statistics for the given simulation's GroupID.
 	Save(groupID simulations.GroupID, score *float64, stats simulations.Statistics) error
 }
