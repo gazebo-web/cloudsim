5c94c8270d2c773ebad5a02dcff1fbd6b87cae38
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-01-21 09:31:28 -0300

    Add job to set websocket connection.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

---
+++ internal/subt/simulator/jobs/set_ws_conn.go
@@ -0,0 +1,51 @@
+package jobs
+
+import (
+	"github.com/jinzhu/gorm"
+	"gitlab.com/ignitionrobotics/web/cloudsim/internal/subt/simulator/state"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/actions"
+	"gitlab.com/ignitionrobotics/web/cloudsim/transport"
+	ignws "gitlab.com/ignitionrobotics/web/cloudsim/transport/ign"
+)
+
+var SetWebsocketConnection = &actions.Job{
+	Name:            "set-ws-conn",
+	PreHooks:        []actions.JobFunc{setStartState},
+	Execute:         connectWebsocket,
+	PostHooks:       []actions.JobFunc{returnState},
+	RollbackHandler: revertWebsocketConnection,
+	InputType:       actions.GetJobDataType(&state.StartSimulation{}),
+	OutputType:      actions.GetJobDataType(&state.StartSimulation{}),
+}
+
+func connectWebsocket(store actions.Store, tx *gorm.DB, deployment *actions.Deployment, value interface{}) (interface{}, error) {
+	s := store.State().(*state.StartSimulation)
+
+	host := s.Platform().Store().Ignition().GetWebsocketHost()
+	path := s.Platform().Store().Ignition().GetWebsocketPath(s.GroupID)
+
+	token, err := s.SubTServices().Simulations().GetWebsocketToken(s.GroupID)
+	if err != nil {
+		return nil, err
+	}
+
+	t, err := ignws.NewIgnWebsocketTransporter(host, path, transport.WebsocketSecureScheme, token)
+	if err != nil {
+		return nil, err
+	}
+
+	s.WebsocketConnection = t
+	store.SetState(s)
+
+	return store, nil
+}
+
+func revertWebsocketConnection(store actions.Store, tx *gorm.DB, deployment *actions.Deployment, value interface{}, err error) (interface{}, error) {
+	s := store.State().(*state.StartSimulation)
+
+	if s.WebsocketConnection != nil && s.WebsocketConnection.IsConnected() {
+		s.WebsocketConnection.Disconnect()
+	}
+
+	return nil, nil
+}
