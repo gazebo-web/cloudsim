c4746465a76cd029421b41697d937a760b0f5b8a
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-01-25 17:45:24 -0300

    Add RunningSimulations comment.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/platform/platform.go
+++ pkg/platform/platform.go
@@ -62,6 +62,7 @@ type platform struct {
 	runningSimulations runsim.Manager
 }
 
+// RunningSimulations returns a runsim.Manager implementation.
 func (p *platform) RunningSimulations() runsim.Manager {
 	return p.runningSimulations
 }
