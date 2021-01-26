2349a526a2911984f143f33f2e5fd8f76888d3df
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-01-26 15:00:54 -0300

    Add users method to Services interface.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/application/services.go
+++ pkg/application/services.go
@@ -2,17 +2,27 @@ package application
 
 import (
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulations"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/users"
 )
 
 // Services groups the services needed by an application to launch simulations.
 type Services interface {
-	// Simulations returns a service to operate over different simulations.
+	// Simulations acts as namespace to access different methods for managing simulations.
 	Simulations() simulations.Service
+
+	// Users acts as a namespace to access different methods for managing users.
+	Users() users.Service
 }
 
 // services is a Services implementation.
 type services struct {
 	simulation simulations.Service
+	users      users.Service
+}
+
+// Users returns the underlying User service.
+func (s *services) Users() users.Service {
+	return s.users
 }
 
 // Simulations returns the underlying Simulation service.
@@ -21,8 +31,9 @@ func (s *services) Simulations() simulations.Service {
 }
 
 // NewServices initializes a new Application Services implementation.
-func NewServices(simulation simulations.Service) Services {
+func NewServices(simulation simulations.Service, users users.Service) Services {
 	return &services{
 		simulation: simulation,
+		users:      users,
 	}
 }
