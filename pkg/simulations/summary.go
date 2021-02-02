50cfda54fef1c76d2b00ec4fa951944de447a4d5
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-02-02 13:59:45 -0300

    Add some documentation to Summary.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/simulations/summary.go
+++ pkg/simulations/summary.go
@@ -2,16 +2,20 @@ package simulations
 
 import "github.com/jinzhu/gorm"
 
-// Summary contains the total score and average statistics for a certain simulation or group of simulations
+// Summary contains the total score and average statistics for a certain simulation or group of simulations.
 type Summary struct {
 	gorm.Model
-	GroupID                *GroupID `json:"-" gorm:"not null;unique"`
-	Score                  float64  `json:"-"`
-	SimTimeDurationAvg     float64  `json:"sim_time_duration_avg"`
-	SimTimeDurationStdDev  float64  `json:"sim_time_duration_std_dev" gorm:"-"`
-	RealTimeDurationAvg    float64  `json:"real_time_duration_avg"`
-	RealTimeDurationStdDev float64  `json:"real_time_duration_std_dev" gorm:"-"`
-	ModelCountAvg          float64  `json:"model_count_avg"`
-	ModelCountStdDev       float64  `json:"model_count_std_dev" gorm:"-"`
-	Sources                string   `json:"-"`
+	// GroupID identifies a simulation.
+	GroupID *GroupID `json:"-" gorm:"not null;unique"`
+	// Score is the simulation score.
+	Score float64 `json:"-"`
+	// SimTimeDurationAvg is the average of the simulation time duration.
+	SimTimeDurationAvg float64 `json:"sim_time_duration_avg"`
+	// SimTimeDurationStdDev is the standard deviation of the simulation time duration.
+	SimTimeDurationStdDev  float64 `json:"sim_time_duration_std_dev" gorm:"-"`
+	RealTimeDurationAvg    float64 `json:"real_time_duration_avg"`
+	RealTimeDurationStdDev float64 `json:"real_time_duration_std_dev" gorm:"-"`
+	ModelCountAvg          float64 `json:"model_count_avg"`
+	ModelCountStdDev       float64 `json:"model_count_std_dev" gorm:"-"`
+	Sources                string  `json:"-"`
 }
