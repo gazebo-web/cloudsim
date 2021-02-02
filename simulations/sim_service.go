c1ca803afcb1a707559abc3202bbfe18d3d64637
German Mas <german@creativa77.com.ar>
2021-02-02 13:40:54 -0300

    Address comments

--- simulations/sim_service.go
+++ simulations/sim_service.go
@@ -2060,7 +2060,7 @@ func (s *Service) GetSimulationDeployment(ctx context.Context, tx *gorm.DB,
 
 	// Check for user permissions if the simulation is private.
 	ok := true
-	if dep.Private != nil && *dep.Private == true {
+	if dep != nil && *dep.Private == true {
 		if user == nil {
 			return nil, ign.NewErrorMessageWithBase(ign.ErrorUnauthorized, err)
 		}
