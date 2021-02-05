e8c82afa140a4869b85d859e2665cf6886270b38
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-02-05 14:51:14 -0300

    Add upload logs job.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

---
+++ internal/subt/simulator/jobs/upload_logs.go
@@ -0,0 +1,114 @@
+package jobs
+
+import (
+	"github.com/jinzhu/gorm"
+	subtapp "gitlab.com/ignitionrobotics/web/cloudsim/internal/subt/application"
+	"gitlab.com/ignitionrobotics/web/cloudsim/internal/subt/simulator/state"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/actions"
+	"gitlab.com/ignitionrobotics/web/ign-go"
+	"path/filepath"
+)
+
+// UploadLogs is a job in charge of uploading simulation logs.
+var UploadLogs = &actions.Job{
+	Name:       "upload-simulation-logs",
+	PreHooks:   []actions.JobFunc{setStopState},
+	Execute:    uploadLogs,
+	PostHooks:  []actions.JobFunc{returnState},
+	InputType:  actions.GetJobDataType(&state.StopSimulation{}),
+	OutputType: actions.GetJobDataType(&state.StopSimulation{}),
+}
+
+// uploadLogsScript is used to set up parameters for the copy script.
+type uploadLogsScript struct {
+	Target   string
+	Filename string
+	Bucket   string
+}
+
+// uploadLogs is the execute function of the UploadLogs job.
+func uploadLogs(store actions.Store, tx *gorm.DB, deployment *actions.Deployment, value interface{}) (interface{}, error) {
+	s := store.State().(*state.StopSimulation)
+
+	// If logs aren't enabled, continue with the rest of the jobs
+	if !s.Platform().Store().Ignition().LogsCopyEnabled() {
+		return s, nil
+	}
+
+	// Get the simulation
+	sim, err := s.Services().Simulations().Get(s.GroupID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get the robot list
+	robots, err := s.Services().Simulations().GetRobots(s.GroupID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get the namespace
+	ns := s.Platform().Store().Orchestrator().Namespace()
+
+	// Get the bucket where to save logs
+	logsBucket := s.Platform().Store().Ignition().LogsBucket()
+
+	// Attempt uploading robot logs
+	for i := range robots {
+		robotID := subtapp.GetRobotID(i)
+		name := subtapp.GetPodNameCommsBridgeCopy(s.GroupID, robotID)
+		res, err := s.Platform().Orchestrator().Pods().Get(name, ns)
+		if err != nil {
+			continue
+		}
+
+		filename := subtapp.GetGazeboLogsFilename(s.GroupID)
+		bucket := filepath.Join(logsBucket, subtapp.GetSimulationLogKey(s.GroupID, *sim.GetOwner()))
+
+		scriptParams := uploadLogsScript{
+			Target:   s.Platform().Store().Ignition().SidecarContainerLogsPath(),
+			Filename: filename,
+			Bucket:   s.Platform().Storage().PrepareAddress(bucket, filename),
+		}
+
+		script, err := ign.ParseTemplate("simulations/scripts/copy_to_s3.sh", scriptParams)
+		if err != nil {
+			continue
+		}
+
+		err = s.Platform().Orchestrator().Pods().Exec(res).Script(subtapp.GetContainerNameCommsBridgeCopy(), script)
+		if err != nil {
+			continue
+		}
+	}
+
+	// Get gazebo copy pod name
+	name := subtapp.GetPodNameGazeboServerCopy(s.GroupID)
+
+	res, err := s.Platform().Orchestrator().Pods().Get(name, ns)
+	if err != nil {
+		return nil, err
+	}
+
+	filename := subtapp.GetGazeboLogsFilename(s.GroupID)
+	bucket := filepath.Join(logsBucket, subtapp.GetSimulationLogKey(s.GroupID, *sim.GetOwner()))
+
+	scriptParams := uploadLogsScript{
+		Target:   s.Platform().Store().Ignition().SidecarContainerLogsPath(),
+		Filename: filename,
+		Bucket:   s.Platform().Storage().PrepareAddress(bucket, filename),
+	}
+
+	script, err := ign.ParseTemplate("simulations/scripts/copy_to_s3.sh", scriptParams)
+	if err != nil {
+		return nil, err
+	}
+
+	// Attempt uploading gazebo logs
+	err = s.Platform().Orchestrator().Pods().Exec(res).Script(subtapp.GetContainerNameGazeboServerCopy(), script)
+	if err != nil {
+		return nil, err
+	}
+
+	return s, nil
+}
