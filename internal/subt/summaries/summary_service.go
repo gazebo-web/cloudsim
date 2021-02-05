9d5c15ba4e9fd9496c84c6f105fe1432ac7c4589
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-02-05 10:58:42 -0300

    Add Calculate method to summary service.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- internal/subt/summaries/summary_service.go
+++ internal/subt/summaries/summary_service.go
@@ -1,6 +1,7 @@
 package summaries
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulations"
 )
@@ -9,11 +10,39 @@ import (
 type Service interface {
 	// Save saves the given score, stats and run data for the given simulation's GroupID.
 	Save(groupID simulations.GroupID, score *float64, stats simulations.Statistics, runData string) (*simulations.Summary, error)
+
+	// Calculate takes a parent group id and returns the calculate summary for all its children.
+	Calculate(groupID simulations.GroupID) (*simulations.Summary, error)
 }
 
 // service is the Summary Service implementation.
 type service struct {
 	db *gorm.DB
+}
+
+// Calculate takes a parent group id and returns the calculate summary for all its children.
+func (s *service) Calculate(groupID simulations.GroupID) (*simulations.Summary, error) {
+	var summary simulations.Summary
+
+	tableName := s.db.NewScope(simulations.Summary{}).TableName()
+	q := s.db.Table(tableName).
+		Select(`SUM(score) AS score,
+			   AVG(sim_time_duration_sec) AS sim_time_duration_avg,
+			   STD(sim_time_duration_sec) AS sim_time_duration_std_dev,
+			   AVG(real_time_duration_sec) AS real_time_duration_avg,
+			   STD(real_time_duration_sec) AS real_time_duration_std_dev,
+			   AVG(model_count) AS model_count_avg,
+			   STD(model_count) AS model_count_std_dev,
+			   GROUP_CONCAT(group_id SEPARATOR ',') AS sources`).
+		Where("group_id LIKE ?", fmt.Sprintf("%%%s%%", groupID)).
+		Where("deleted_at IS NULL").
+		Scan(&summary)
+
+	if err := q.Error; err != nil {
+		return nil, err
+	}
+
+	return &summary, nil
 }
 
 // Save saves the given score, stats and run data for the given simulation's GroupID.
