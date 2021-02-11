a2a7b466c87b5e51f564d349af69a9c7ff37967a
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-02-11 17:39:43 -0300

    Finish mock EC2 implementation.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/mock/ec2.go
+++ pkg/mock/ec2.go
@@ -6,6 +6,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
+	"strings"
 )
 
 // ec2api is an ec2iface.EC2API implementation.
@@ -28,27 +29,73 @@ func (e *ec2api) RunInstances(input *ec2.RunInstancesInput) (*ec2.Reservation, e
 	}
 
 	var i int64
+
+	var instances []*ec2.Instance
+
 	for i = 0; i < *input.MaxCount; i++ {
 		id := make([]byte, 10)
 		if _, err := rand.Read(id); err != nil {
 			return nil, err
 		}
-		e.Instances[string(id)] = ec2.Instance{}
+
+		var instance ec2.Instance
+
+		for _, tag := range input.TagSpecifications {
+			instance.Tags = append(instance.Tags, tag.Tags...)
+		}
+
+		instances = append(instances, &instance)
+
+		e.Instances[string(id)] = instance
 	}
-	return &ec2.Reservation{}, nil
+
+	return &ec2.Reservation{
+		Instances: instances,
+	}, nil
 }
 
 // TerminateInstances mocks TerminateInstances.
 func (e *ec2api) TerminateInstances(input *ec2.TerminateInstancesInput) (*ec2.TerminateInstancesOutput, error) {
+	for _, id := range input.InstanceIds {
+		delete(e.Instances, *id)
+	}
 	return &ec2.TerminateInstancesOutput{}, nil
 }
 
 // DescribeInstances mocks DescribeInstances.
 func (e *ec2api) DescribeInstances(input *ec2.DescribeInstancesInput) (*ec2.DescribeInstancesOutput, error) {
-	return nil, nil
+
+	var found []*ec2.Reservation
+	if len(input.Filters) == 0 {
+		return &ec2.DescribeInstancesOutput{
+			Reservations: nil,
+		}, nil
+	}
+
+	for _, filter := range input.Filters {
+		if !strings.Contains(*filter.Name, "tag:") {
+			continue
+		}
+		tag := strings.Replace(*filter.Name, "tag", "", -1)
+		for _, instance := range e.Instances {
+			for _, t := range instance.Tags {
+				if *t.Key == tag {
+					found = append(found, &ec2.Reservation{
+						Instances: []*ec2.Instance{
+							&instance,
+						},
+					})
+				}
+			}
+		}
+	}
+
+	return &ec2.DescribeInstancesOutput{
+		Reservations: found,
+	}, nil
 }
 
 // WaitUntilInstanceStatusOk mocks WaitUntilInstanceStatusOk.
-func (e *ec2api) WaitUntilInstanceStatusOk(*ec2.DescribeInstanceStatusInput) error {
+func (e *ec2api) WaitUntilInstanceStatusOk(input *ec2.DescribeInstanceStatusInput) error {
 	return nil
 }
