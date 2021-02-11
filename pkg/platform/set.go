70aaeba8cbbd8b9751307ba02b6727123c54c7a8
Alejo Carballude <alejocarballude@gmail.com>
2021-02-11 10:26:16 -0300

    Add platform set.

---
+++ pkg/platform/set.go
@@ -0,0 +1,72 @@
+package platform
+
+import "errors"
+
+var (
+	// ErrPlatformNotFound is returned when a platform for a provided selector cannot be found.
+	ErrPlatformNotFound = errors.New("platform not found")
+)
+
+// Selector is used to uniquely identify a Platform.
+type Selector string
+
+// Set contains a set of platforms that can be used by Cloudsim.
+// Implementations must be simple platform containers and must not be aware of the feature sets, differences (if any) or
+// implementation details of the available platforms they contain. This is by design, in order to give applications the
+// flexibility to manage sets of platforms as they see fit.
+// All contained platforms are completely independent from each other. As an example, you can have a set of platforms
+// that use the same internal components (e.g. AWS+Kubernetes), but that are configured to point to different
+// service regions. At the same time, it is possible to have additional platforms making use of entirely different
+// components (e.g. GCP+Mesos, Azure+Swarm) co-exist with the set of AWS+Kubernetes platforms.
+// Every platform is uniquely identified by a Selector, a user-defined identifier. To make use of a specific platform,
+// the target platform's Selector is passed to the Set implementation through the Platform method.
+// An example of a selector set:
+//   * aws_k8s_us_east_1 - Platform containing AWS and Kubernetes components pointed at us-east-1.
+//   * aws_k8s_us_east_2 - Platform containing AWS and Kubernetes components pointed at us-east-2.
+//   * gcp_mesos - Platform containing GCE and Apache Mesos components.
+//   * azure_swarm - Platform containing Azure and Docker Swarm components.
+type Set interface {
+	// Selectors returns a slice with all the available platform selectors.
+	Selectors() []Selector
+	// Set returns a slice with all the available Map.
+	Platforms() []Platform
+	// Set returns the platform that matches a specific selector, or an error if it is not found.
+	Platform(selector Selector) (Platform, error)
+}
+
+// Map is the default Set implementation.
+type Map map[Selector]Platform
+
+// Selectors returns a slice with all the available platform selectors.
+func (p Map) Selectors() []Selector {
+	selectors := make([]Selector, len(p))
+	i := 0
+	for selector := range p {
+		selectors[i] = selector
+		i++
+	}
+
+	return selectors
+}
+
+// Platforms returns a slice with all the available platforms.
+func (p Map) Platforms() []Platform {
+	platforms := make([]Platform, len(p))
+	i := 0
+	for _, platform := range p {
+		platforms[i] = platform
+		i++
+	}
+
+	return platforms
+}
+
+// Platform returns receives a selector and returns its matching platform or an error if it is not found.
+func (p Map) Platform(selector Selector) (Platform, error) {
+	platform, ok := p[selector]
+	if !ok {
+		return nil, ErrPlatformNotFound
+	}
+
+	return platform, nil
+}
---
+++ pkg/platform/set_test.go
@@ -0,0 +1,70 @@
+package platform
+
+import (
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+func TestMapSuite(t *testing.T) {
+	suite.Run(t, new(testMapSuite))
+}
+
+type testMapSuite struct {
+	suite.Suite
+	selector1 Selector
+	selector2 Selector
+	selector3 Selector
+	platform1 Platform
+	platform2 Platform
+	platform3 Platform
+	set       Set
+}
+
+func (s *testMapSuite) SetupSuite() {
+	s.selector1 = "test_1"
+	s.platform1 = NewPlatform(Components{})
+
+	s.selector2 = "test_2"
+	s.platform2 = NewPlatform(Components{})
+
+	s.selector3 = "test_3"
+	s.platform3 = NewPlatform(Components{})
+
+	s.set = Map{
+		s.selector1: s.platform1,
+		s.selector2: s.platform2,
+		s.selector3: s.platform3,
+	}
+}
+
+func (s *testMapSuite) TestSelectors() {
+	selectors := s.set.Selectors()
+	expected := []Selector{s.selector1, s.selector2, s.selector3}
+	s.ElementsMatch(expected, selectors)
+}
+
+func (s *testMapSuite) TestPlatforms() {
+	platforms := s.set.Platforms()
+	expected := []Platform{s.platform1, s.platform2, s.platform3}
+	s.ElementsMatch(expected, platforms)
+}
+
+func (s *testMapSuite) TestPlatformValidSelector() {
+	// Get the first platform
+	platform, err := s.set.Platform(s.selector1)
+	s.NoError(err)
+	s.Equal(s.platform1, platform)
+
+	// Get the third platform
+	platform, err = s.set.Platform(s.selector3)
+	s.NoError(err)
+	s.Equal(s.platform3, platform)
+}
+
+func (s *testMapSuite) TestPlatformInvalidSelector() {
+	// Provide an invalid selector
+	selector := Selector("invalid")
+	platform, err := s.set.Platform(selector)
+	s.EqualError(err, ErrPlatformNotFound.Error())
+	s.Nil(platform)
+}
