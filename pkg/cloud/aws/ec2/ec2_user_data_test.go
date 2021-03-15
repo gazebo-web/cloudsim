86fd3e9c26d7bded73bf77f2b2b37657a80a4ab2
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-03-15 10:35:01 -0300

    Add script assertion.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/cloud/aws/ec2/ec2_user_data_test.go
+++ pkg/cloud/aws/ec2/ec2_user_data_test.go
@@ -2,7 +2,7 @@ package ec2
 
 import (
 	"bytes"
-	"fmt"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"testing"
 	"text/template"
@@ -22,5 +22,18 @@ func TestUserDataScript(t *testing.T) {
 	})
 	require.NoError(t, err)
 
-	fmt.Println("Script:", buffer.String())
+	const expected = `#!/bin/bash
+set -x
+exec > >(tee /var/log/user-data.log|logger -t user-data ) 2>&1
+echo BEGIN
+date '+%Y-%m-%d %H:%M:%S'
+cat > /etc/systemd/system/kubelet.service.d/20-labels-taints.conf <<EOF
+[Service]
+Environment="KUBELET_EXTRA_ARGS=--node-labels=app=test,example=test"
+EOF
+set -o xtrace
+/etc/eks/bootstrap.sh testing-cluster-name --use-max-pods false
+`
+
+	assert.Equal(t, expected, buffer.String())
 }
