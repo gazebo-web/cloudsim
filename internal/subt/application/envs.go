ed105b4b56739be0488afffcb2233bfbb04f82f0
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-03-29 14:39:44 -0300

    Move IGN_IP to list of env vars from a certain source.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- internal/subt/application/envs.go
+++ internal/subt/application/envs.go
@@ -13,7 +13,6 @@ func GetEnvVarsCommsBridge(groupID simulations.GroupID, robotName, gzServerIP, v
 		"IGN_RELAY":      gzServerIP,
 		"IGN_VERBOSE":    verbosity,
 		"ROBOT_NAME":     robotName,
-		"IGN_IP":         "", // To be removed.
 		"ROS_MASTER_URI": "http://$(ROS_IP):11311",
 	}
 }
@@ -24,6 +23,7 @@ func GetEnvVarsCommsBridge(groupID simulations.GroupID, robotName, gzServerIP, v
 func GetEnvVarsFromSourceCommsBridge() map[string]string {
 	return map[string]string{
 		"ROS_IP": orchestrator.EnvVarSourcePodIP,
+		"IGN_IP": orchestrator.EnvVarSourcePodIP,
 	}
 }
 
