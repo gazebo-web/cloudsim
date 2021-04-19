f008ea2b78762c7df5e6da8f54f37e755a3d54f1
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-04-19 14:53:06 -0300

    Add namespace argument to List method.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/orchestrator/pods.go
+++ pkg/orchestrator/pods.go
@@ -142,7 +142,7 @@ type Pods interface {
 	Delete(resource Resource) (Resource, error)
 	Get(name, namespace string) (Resource, error)
 	GetIP(name, namespace string) (string, error)
-	List(selector Selector) ([]Resource, error)
+	List(namespace string, selector Selector) ([]Resource, error)
 }
 
 // Executor groups a set of methods to run commands and scripts inside a Pod.
