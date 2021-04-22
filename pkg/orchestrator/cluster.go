a3e40021148798e0abdb53e3040f0c68efd40b32
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-04-22 17:49:51 -0300

    Implement Resource interface in the resource struct.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/orchestrator/cluster.go
+++ pkg/orchestrator/cluster.go
@@ -1,26 +1,5 @@
 package orchestrator
 
-import "time"
-
-// Condition represents a state that should be reached.
-type Condition struct {
-	Type   string
-	Status string
-}
-
-var (
-	// ReadyCondition is used to indicate that Nodes and Pods are ready.
-	ReadyCondition = Condition{
-		Type:   "Ready",
-		Status: "True",
-	}
-	// HasIPStatusCondition is used to indicate that pods have ips available.
-	HasIPStatusCondition = Condition{
-		Type:   "HasIPStatus",
-		Status: "True",
-	}
-)
-
 // Cluster groups a set of methods for managing a cluster.
 type Cluster interface {
 	Nodes() Nodes
@@ -29,32 +8,4 @@ type Cluster interface {
 	Ingresses() Ingresses
 	IngressRules() IngressRules
 	NetworkPolicies() NetworkPolicies
-}
-
-// Selector is used to identify a certain resource.
-type Selector interface {
-	// String returns the selector represented in string format.
-	String() string
-	// Map returns the underlying selector's map.
-	Map() map[string]string
-	// Extend extends the underlying base map with the extension selector.
-	// NOTE: If a certain key already exists in the base map, it will be overwritten by the extension value.
-	Extend(extension Selector) Selector
-	// Set sets the given value to the given key. If the key already exists, it will be overwritten.
-	Set(key string, value string)
-}
-
-// Resource groups a set of method to identify a resource in a cluster.
-type Resource interface {
-	// Name returns the name of the Resource
-	Name() string
-	// Selector returns the Resource's Selector.
-	Selector() Selector
-	// Namespace returns the namespace where the Resource lives in.
-	Namespace() string
-	// CreationTimestamp is a timestamp representing the server time when this object was created.
-	CreationTimestamp() time.Time
-	// DeletionTimestamp is a timestamp at which this resource will be deleted. This field is set by the server when a
-	// graceful deletion is requested.
-	DeletionTimestamp() *time.Time
 }
--- pkg/orchestrator/types.go
+++ pkg/orchestrator/types.go
@@ -3,7 +3,81 @@ package orchestrator
 import (
 	"fmt"
 	"strings"
+	"time"
 )
+
+// Condition represents a state that should be reached.
+type Condition struct {
+	Type   string
+	Status string
+}
+
+var (
+	// ReadyCondition is used to indicate that Nodes and Pods are ready.
+	ReadyCondition = Condition{
+		Type:   "Ready",
+		Status: "True",
+	}
+	// HasIPStatusCondition is used to indicate that pods have ips available.
+	HasIPStatusCondition = Condition{
+		Type:   "HasIPStatus",
+		Status: "True",
+	}
+)
+
+// Phase represents a certain point in the lifecycle of a Resource.
+type Phase string
+
+const (
+	// PhasePending is used to represent when a Resource is on a Pending Phase.
+	// Used by: Pods, Nodes.
+	PhasePending Phase = "Pending"
+	// PhaseRunning is used to represent when a Resource is on a Running Phase.
+	// Used by Pods, Nodes.
+	PhaseRunning Phase = "Running"
+	// PhaseSucceeded is used to represent when a Resource is on a Succeeded Phase.
+	// Used by: Pods.
+	PhaseSucceeded Phase = "Succeeded"
+	// PhaseFailed is used to represent when a Resource is on a Failed Phase.
+	// Used by: Pods.
+	PhaseFailed Phase = "Failed"
+	// PhaseUnknown is used to represent when a Resource is on a Unknown Phase.
+	// Used by: Pods.
+	PhaseUnknown Phase = "Unknown"
+	// PhaseTerminated is used to represent when a Resource is on a Terminated Phase.
+	// Used by: Nodes.
+	PhaseTerminated Phase = "Terminated"
+)
+
+// Selector is used to identify a certain resource.
+type Selector interface {
+	// String returns the selector represented in string format.
+	String() string
+	// Map returns the underlying selector's map.
+	Map() map[string]string
+	// Extend extends the underlying base map with the extension selector.
+	// NOTE: If a certain key already exists in the base map, it will be overwritten by the extension value.
+	Extend(extension Selector) Selector
+	// Set sets the given value to the given key. If the key already exists, it will be overwritten.
+	Set(key string, value string)
+}
+
+// Resource groups a set of method to identify a resource in a cluster.
+type Resource interface {
+	// Name returns the name of the Resource
+	Name() string
+	// Selector returns the Resource's Selector.
+	Selector() Selector
+	// Namespace returns the namespace where the Resource lives in.
+	Namespace() string
+	// CreationTimestamp is a timestamp representing the server time when this object was created.
+	CreationTimestamp() time.Time
+	// DeletionTimestamp is a timestamp at which this resource will be deleted. This field is set by the server when a
+	// graceful deletion is requested.
+	DeletionTimestamp() *time.Time
+	// Phase is a simple, high-level summary of where the Resource is in its lifecycle.
+	Phase() Phase
+}
 
 // resource is an orchestrator.Resource implementation of Kubernetes resources.
 type resource struct {
@@ -13,6 +87,29 @@ type resource struct {
 	selector Selector
 	// namespace is the environment where the resource is currently running.
 	namespace string
+	// phase is a simple, high-level summary of where the Resource is in its lifecycle.
+	phase Phase
+	// creationTimestamp is a timestamp representing the server time when this object was created.
+	creationTimestamp time.Time
+	// deletionTimestamp is a timestamp at which this resource will be deleted. This field is set by the server when a
+	// graceful deletion is requested.
+	deletionTimestamp *time.Time
+}
+
+// CreationTimestamp is a timestamp representing the server time when this object was created.
+func (s *resource) CreationTimestamp() time.Time {
+	return s.creationTimestamp
+}
+
+// DeletionTimestamp is a timestamp at which this resource will be deleted. This field is set by the server when a
+// graceful deletion is requested.
+func (s *resource) DeletionTimestamp() *time.Time {
+	return s.deletionTimestamp
+}
+
+// Phase is a simple, high-level summary of where the Resource is in its lifecycle.
+func (s *resource) Phase() Phase {
+	return s.phase
 }
 
 // Name returns the resource's name.
@@ -28,6 +125,15 @@ func (s *resource) Selector() Selector {
 // Namespace returns the resource's namespace.
 func (s *resource) Namespace() string {
 	return s.namespace
+}
+
+type ResourceOptions struct {
+	Name              string
+	Namespace         string
+	Selector          Selector
+	Phase             Phase
+	CreationTimestamp time.Time
+	DeletionTimestamp *time.Time
 }
 
 // NewResource initializes a new orchestrator.Resource using a kubernetes service implementation.
