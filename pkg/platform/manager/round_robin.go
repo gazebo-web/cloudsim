9625f88df80fc7446752f8d6f8d0cde40143940d
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-06-04 13:45:19 -0300

    Make round robin implementation private.
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/platform/manager/round_robin.go
+++ pkg/platform/manager/round_robin.go
@@ -14,26 +14,26 @@ func WithRoundRobin(base Manager, err error) (Manager, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &RoundRobin{
+	return &roundRobin{
 		iterator: c,
 		manager:  base,
 	}, nil
 }
 
-// RoundRobin is a Manager implementation using round robin technique.
-type RoundRobin struct {
+// roundRobin is a Manager implementation using round robin technique.
+type roundRobin struct {
 	iterator cycler.Cycler
 	manager  Manager
 }
 
 // Selectors returns a slice with all the available platform selectors.
-func (c *RoundRobin) Selectors() []string {
+func (c *roundRobin) Selectors() []string {
 	return c.manager.Selectors()
 }
 
 // Platforms returns a slice with all the available manager, but compared to Map.Platforms, it will try to
 // return a different platform every time at the index 0 if no selector is passed using round robin.
-func (c *RoundRobin) Platforms(selector *string) []platform.Platform {
+func (c *roundRobin) Platforms(selector *string) []platform.Platform {
 	if selector == nil {
 		next := c.iterator.Next().(string)
 		return c.manager.Platforms(&next)
@@ -42,6 +42,6 @@ func (c *RoundRobin) Platforms(selector *string) []platform.Platform {
 }
 
 // Platform receives a selector and returns its matching platform or an error if it is not found.
-func (c *RoundRobin) Platform(selector string) (platform.Platform, error) {
+func (c *roundRobin) Platform(selector string) (platform.Platform, error) {
 	return c.manager.Platform(selector)
 }
