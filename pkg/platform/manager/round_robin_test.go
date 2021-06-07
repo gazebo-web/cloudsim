e028c72d6ab0dd3879be4185f6ae5d26c837e25b
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-06-07 10:37:24 -0300

    Remove duplicated tests
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/platform/manager/round_robin_test.go
+++ pkg/platform/manager/round_robin_test.go
@@ -36,20 +36,8 @@ func (s *testRoundRobinSuite) SetupSuite() {
 		s.selector1: s.platform1,
 		s.selector2: s.platform2,
 		s.selector3: s.platform3,
-	}, nil)
+	})
 	s.Require().NoError(err)
-}
-
-func (s *testRoundRobinSuite) TestSelectors() {
-	selectors := s.platformRoundRobin.Selectors()
-	expected := []string{s.selector1, s.selector2, s.selector3}
-	s.Require().ElementsMatch(expected, selectors)
-}
-
-func (s *testRoundRobinSuite) TestPlatformsNoSelector() {
-	platforms := s.platformRoundRobin.Platforms(nil)
-	expected := []platform.Platform{s.platform1, s.platform2, s.platform3}
-	s.Require().ElementsMatch(expected, platforms)
 }
 
 func (s *testRoundRobinSuite) TestPlatformsNoSelectorMultipleCalls() {
@@ -64,38 +52,4 @@ func (s *testRoundRobinSuite) TestPlatformsNoSelectorMultipleCalls() {
 	platforms = s.platformRoundRobin.Platforms(nil)
 	expected = []platform.Platform{s.platform3, s.platform1, s.platform2}
 	s.Assert().ElementsMatch(expected, platforms)
-}
-
-func (s *testRoundRobinSuite) TestPlatformsValidSelector() {
-	platforms := s.platformRoundRobin.Platforms(&s.selector2)
-	s.Require().Equal(s.platform2, platforms[0])
-	expected := []platform.Platform{s.platform1, s.platform2, s.platform3}
-	s.Require().ElementsMatch(expected, platforms)
-}
-
-func (s *testRoundRobinSuite) TestPlatformsInvalidSelector() {
-	selector := "invalid"
-	platforms := s.platformRoundRobin.Platforms(&selector)
-	expected := []platform.Platform{s.platform1, s.platform2, s.platform3}
-	s.Require().ElementsMatch(expected, platforms)
-}
-
-func (s *testRoundRobinSuite) TestPlatformValidSelector() {
-	// Get the first platform
-	platform, err := s.platformRoundRobin.Platform(s.selector1)
-	s.Require().NoError(err)
-	s.Require().Equal(s.platform1, platform)
-
-	// Get the third platform
-	platform, err = s.platformRoundRobin.Platform(s.selector3)
-	s.Require().NoError(err)
-	s.Require().Equal(s.platform3, platform)
-}
-
-func (s *testRoundRobinSuite) TestPlatformInvalidSelector() {
-	// Provide an invalid selector
-	selector := "invalid"
-	platform, err := s.platformRoundRobin.Platform(selector)
-	s.Assert().EqualError(err, ErrPlatformNotFound.Error())
-	s.Assert().Nil(platform)
 }
