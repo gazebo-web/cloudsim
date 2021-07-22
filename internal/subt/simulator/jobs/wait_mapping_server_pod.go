89d043925cad34b1593a776cd78fc996b1279cfb
Alejo Carballude <alejocarballude@gmail.com>
2021-07-22 22:58:10 +0000

    Release 2.5.2

--- internal/subt/simulator/jobs/wait_mapping_server_pod.go
+++ internal/subt/simulator/jobs/wait_mapping_server_pod.go
@@ -2,9 +2,12 @@ package jobs
 
 import (
 	"github.com/jinzhu/gorm"
+	"github.com/pkg/errors"
 	subtapp "gitlab.com/ignitionrobotics/web/cloudsim/internal/subt/application"
+	"gitlab.com/ignitionrobotics/web/cloudsim/internal/subt/simulations"
 	"gitlab.com/ignitionrobotics/web/cloudsim/internal/subt/simulator/state"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/actions"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator/components/pods"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator/resource"
 	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/simulator/jobs"
 )
@@ -13,7 +16,7 @@ import (
 var WaitForMappingServerPod = jobs.Wait.Extend(actions.Job{
 	Name:       "wait-mapping-server-pod",
 	PreHooks:   []actions.JobFunc{createWaitRequestMappingServerPod},
-	PostHooks:  []actions.JobFunc{checkWaitError, returnState},
+	PostHooks:  []actions.JobFunc{checkMappingServerWaitError, returnState},
 	InputType:  actions.GetJobDataType(&state.StartSimulation{}),
 	OutputType: actions.GetJobDataType(&state.StartSimulation{}),
 })
@@ -43,4 +46,37 @@ func createWaitRequestMappingServerPod(store actions.Store, tx *gorm.DB, deploym
 		PollFrequency: pollFreq,
 		Timeout:       timeout,
 	}, nil
+}
+
+// checkMappingServerWaitError validates the Wait return value.
+// If the track does not have a mapping server image defined, the Missing Pods errors is ignored.
+// In any other case, an error will result in a rollback.
+func checkMappingServerWaitError(store actions.Store, tx *gorm.DB, deployment *actions.Deployment, value interface{}) (interface{}, error) {
+	s := store.State().(*state.StartSimulation)
+
+	// Get simulation
+	sim, err := s.Services().Simulations().Get(s.GroupID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse to subt simulation
+	subtSim := sim.(simulations.Simulation)
+
+	// Get track
+	track, err := s.SubTServices().Tracks().Get(subtSim.GetTrack(), subtSim.GetWorldIndex(), subtSim.GetRunIndex())
+	if err != nil {
+		return nil, err
+	}
+
+	output := value.(jobs.WaitOutput)
+
+	// Ignore missing pods error if mapping image is not defined.
+	if track.MappingImage == nil && errors.Is(output.Error, pods.ErrMissingPods) {
+		return jobs.LaunchPodsInput{}, nil
+	} else if output.Error != nil {
+		return nil, output.Error
+	}
+
+	return nil, nil
 }
