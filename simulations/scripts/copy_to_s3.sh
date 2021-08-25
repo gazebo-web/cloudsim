5ac7d57ce2cd17bd6193d2c121e8694ab0df063c
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-08-25 15:15:45 -0300

    Remove subt-related files
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- simulations/scripts/copy_to_s3.sh
+++
@@ -1,9 +0,0 @@
-FILEPATH={{ .Target }}
-FILENAME={{ .Filename }}
-if [ -f "${FILEPATH}" ]; then
-  ln -s ${FILEPATH} /tmp/${FILENAME}
-else
-  cd ${FILEPATH}
-  tar czf /tmp/${FILENAME} *
-fi
-aws s3 cp /tmp/${FILENAME} {{ .Bucket }}
