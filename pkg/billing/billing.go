8eed74bc435b20e03c18df2e68c74dc69bbb0e11
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-11-10 17:29:21 -0300

    Implement get balance method
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/billing/billing.go
+++ pkg/billing/billing.go
@@ -2,12 +2,19 @@ package billing
 
 import (
 	"context"
+	apiCredits "gitlab.com/ignitionrobotics/billing/credits/pkg/api"
+	credits "gitlab.com/ignitionrobotics/billing/credits/pkg/client"
+	payments "gitlab.com/ignitionrobotics/billing/payments/pkg/client"
 	"gitlab.com/ignitionrobotics/web/fuelserver/bundles/users"
+	"net/url"
+	"time"
 )
 
 type Config struct {
-	CreditsURL  string
-	PaymentsURL string
+	CreditsURL      string
+	PaymentsURL     string
+	ApplicationName string
+	Timeout         time.Duration
 }
 
 type Service interface {
@@ -15,12 +22,33 @@ type Service interface {
 }
 
 type service struct {
+	payments        payments.Client
+	credits         credits.Client
+	applicationName string
 }
 
 func (s *service) GetBalance(ctx context.Context, user *users.User) (interface{}, error) {
-	panic("implement me")
+	return s.credits.GetBalance(ctx, apiCredits.GetBalanceRequest{
+		Handle:      *user.Username,
+		Application: s.applicationName,
+	})
 }
 
-func NewService(cfg Config) Service {
-	return &service{}
+func NewService(cfg Config) (Service, error) {
+	u, err := url.Parse(cfg.PaymentsURL)
+	if err != nil {
+		return nil, err
+	}
+	p := payments.NewPaymentsClientV1(u, cfg.Timeout)
+
+	u, err = url.Parse(cfg.CreditsURL)
+	if err != nil {
+		return nil, err
+	}
+	c := credits.NewCreditsClientV1(u, cfg.Timeout)
+	return &service{
+		payments:        p,
+		credits:         c,
+		applicationName: cfg.ApplicationName,
+	}, nil
 }
