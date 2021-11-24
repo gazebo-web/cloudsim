351bee0100f775791f08bdb04c46cbf51794a16b
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-11-24 14:20:22 -0300

    Add AWS calculator with EC2 price parser
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

---
+++ pkg/cloud/aws/calculator.go
@@ -0,0 +1,81 @@
+package aws
+
+import (
+	"errors"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/pricing"
+	"github.com/aws/aws-sdk-go/service/pricing/pricingiface"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/calculator"
+)
+
+const (
+	KindMachines = "AmazonEC2"
+	KindStorage  = "AmazonS3"
+)
+
+// PriceParser parses a given AWS price result from the Pricing API and returns a calculator.Rate value.
+// Different implementations depending on the response of the Pricing API can be added using this function signature.
+// EC2 example: ParseEC2
+type PriceParser func(price aws.JSONValue) (calculator.Rate, error)
+
+// costCalculator provides an AWS calculator for different services.
+type costCalculator struct {
+	// API holds a reference to a pricingiface.PricingAPI implementation.
+	API         pricingiface.PricingAPI
+	priceParser PriceParser
+}
+
+func (c *costCalculator) CalculateCost(resources []calculator.Resource) (calculator.Rate, error) {
+	rates := make([]calculator.Rate, len(resources))
+	for i, res := range resources {
+		rate, err := c.calculateRate(res)
+		if err != nil {
+			return calculator.Rate{}, err
+		}
+		rates[i] = rate
+	}
+	return calculator.SumRates(rates), nil
+}
+
+func (c *costCalculator) calculateRate(res calculator.Resource) (calculator.Rate, error) {
+	list, err := c.API.GetProducts(&pricing.GetProductsInput{
+		FormatVersion: aws.String("aws_v1"),
+		MaxResults:    aws.Int64(1),
+		ServiceCode:   aws.String(res.Kind),
+		Filters:       c.convertResourceToFilters(res),
+	})
+	if err != nil {
+		return calculator.Rate{}, err
+	}
+	if len(list.PriceList) == 0 {
+		return calculator.Rate{}, errors.New("product not found")
+	}
+	rate, err := c.priceParser(list.PriceList[0])
+	if err != nil {
+		return calculator.Rate{}, err
+	}
+	return rate, nil
+}
+
+func (c *costCalculator) convertResourceToFilters(resource calculator.Resource) []*pricing.Filter {
+	filters := make([]*pricing.Filter, 0, len(resource.Values))
+	for k, v := range resource.Values {
+		value, ok := v.(string)
+		if !ok {
+			continue
+		}
+		filters = append(filters, &pricing.Filter{
+			Field: aws.String(k),
+			Type:  aws.String(pricing.FilterTypeTermMatch),
+			Value: &value,
+		})
+	}
+	return filters
+}
+
+func NewCostCalculator(api pricingiface.PricingAPI, priceParser PriceParser) calculator.CostCalculator {
+	return &costCalculator{
+		API:         api,
+		priceParser: priceParser,
+	}
+}
---
+++ pkg/cloud/aws/price_parser_ec2.go
@@ -0,0 +1,89 @@
+package aws
+
+import (
+	"errors"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/itchyny/gojq"
+	"github.com/mitchellh/mapstructure"
+	"gitlab.com/ignitionrobotics/web/cloudsim/pkg/calculator"
+	"math"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// priceEC2 is a data structure used as a helper to represent
+type priceEC2 struct {
+	// Frequency is the frequency at which a certain EC2 machines gets charged at.
+	// Values: Hrs
+	Frequency string `json:"frequency"`
+
+	// Amounts groups the currencies and the respective amounts of money that a certain EC2 machines can be charged for.
+	Amounts []struct {
+		// Currency holds the currency code
+		Currency string `json:"currency"`
+		// Amount is the amount of money to charge.
+		Amount string `json:"amount"`
+	} `json:"amounts"`
+}
+
+// ParseEC2 is a PriceParser func used for parsing EC2 pricing. It reads the given product definition and returns
+// a rate at which the given product should be charged of.
+// If multiple currencies are present, it returns the first one it finds.
+func ParseEC2(product aws.JSONValue) (calculator.Rate, error) {
+	q, err := gojq.Parse("{frequency: .terms.OnDemand[].priceDimensions[].unit, amounts: .terms.OnDemand[].priceDimensions[].pricePerUnit | to_entries | map_values({ currency: .key, amount: .value }) }")
+	if err != nil {
+		return calculator.Rate{}, err
+	}
+
+	iter := q.Run(map[string]interface{}(product))
+	v, ok := iter.Next()
+	if !ok {
+		return calculator.Rate{}, errors.New("failed to parse JSON using jq")
+	}
+
+	p, err := convertMapToPriceEC2(v)
+	if err != nil {
+		return calculator.Rate{}, err
+	}
+
+	amount, err := strconv.ParseFloat(p.Amounts[0].Amount, 64)
+	if err != nil {
+		return calculator.Rate{}, err
+	}
+
+	return calculator.Rate{
+		Amount:    normalizeAmount(amount),
+		Currency:  normalizeCurrency(p.Amounts[0].Currency),
+		Frequency: normalizeFrequency(p.Frequency),
+	}, nil
+}
+
+// normalizeFrequency converts the given unit of time from AWS to time.Duration.
+// It defaults to time.Hour.
+func normalizeFrequency(timeUnit string) time.Duration {
+	switch timeUnit {
+	case "Hrs":
+		return time.Hour
+	}
+	return time.Hour
+}
+
+// normalizeCurrency converts the given currency into a ISO 1472 currency code in lowercase format.
+func normalizeCurrency(currency string) string {
+	return strings.ToLower(currency)
+}
+
+// normalizeAmount converts the given amount to cents in USD.
+func normalizeAmount(amount float64) uint {
+	return uint(math.Round(amount * 100))
+}
+
+// convertMapToPriceEC2 decodes the given map as a priceEC2 structure.
+func convertMapToPriceEC2(v interface{}) (priceEC2, error) {
+	var p priceEC2
+	if err := mapstructure.Decode(v, &p); err != nil {
+		return priceEC2{}, err
+	}
+	return p, nil
+}
