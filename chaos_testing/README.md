8b096e00474f808b1b46b7f6f093480f5184db69
Marcos Huck <marcos.h3012@gmail.com>
2022-01-04 10:59:34 +0000

    Release 3.0.0 - Cloudsim library

--- chaos_testing/README.md
+++
@@ -1,5 +0,0 @@
-# Chaos Testing
-
-This directory contains utilities for chaos testing.
-
-1. `shaper`: A tool for network traffic manipulation (shaping).
--- pkg/transport/message.go
+++ pkg/transport/message.go
@@ -2,6 +2,6 @@ package transport
 
 // Message represents a set of methods that message structs must implement to be used in a transport.
 type Message interface {
-	// Get returns the message payload.
+	// GetPayload returns the message payload.
 	GetPayload(out interface{}) error
 }
