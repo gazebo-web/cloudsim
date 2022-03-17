5827b4480549b7e0c22c2160d9ee614ca8c3a1df
Marcos Huck <marcoshuck@ekumenlabs.com>
2022-03-17 17:54:43 -0300

    Change CRUD operations to make them bulk operations
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -2,14 +2,13 @@ package repository
 
 // Repository holds methods to CRUD an entity on a certain persistence layer.
 type Repository interface {
-	// Create saves the given input entity in a persistence layer.
-	// The result is written back on the input.
-	Create(in interface{}) error
+	// CreateBulk saves the given set of input entities in a persistence layer.
+	CreateBulk(in ...interface{}) error
 	// Get writes on out the data of the entity identified by id.
 	Get(out interface{}, id uint) error
-	// Update updates an entity identified by id with the input data.
-	// The result is written back on the input.
-	Update(in interface{}, id uint) error
-	// Remove removes the entity identified by id and writes on out the data of the element that has been removed.
-	Remove(out interface{}, id uint) error
+	// UpdateBulk overwrites the information given as the input on the different entities
+	// identified by the given ids.
+	UpdateBulk(in interface{}, ids ...uint) error
+	// RemoveBulk removes the entities identified by the given ids.
+	RemoveBulk(ids ...uint) error
 }
