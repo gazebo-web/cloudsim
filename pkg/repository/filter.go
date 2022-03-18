9db35343ce5e33dd4ed03673f8d9ee69170f4795
Marcos Huck <marcoshuck@ekumenlabs.com>
2022-03-18 10:39:20 -0300

    Replace filter interface with struct
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/repository/filter.go
+++ pkg/repository/filter.go
@@ -1,11 +1,11 @@
 package repository
 
 // Filter is used by a Repository to filter data by a key-value set.
-type Filter interface {
+type Filter struct {
 	// Template represents a SQL Syntax.
 	// Example: `name = ? AND age = ?`
-	Template() string
+	Template string
 	// Values returns the values used by the SQL Syntax.
 	// Example: `["Test", 33]`
-	Values() []interface{}
+	Values []interface{}
 }
