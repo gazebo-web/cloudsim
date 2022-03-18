367d3c35171baa643f6639a741164bae26a4165a
Marcos Huck <marcoshuck@ekumenlabs.com>
2022-03-18 10:39:29 -0300

    Add docs to repository interface
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -15,10 +15,16 @@ var (
 
 // Repository holds methods to CRUD an entity on a certain persistence layer.
 type Repository interface {
+	// Create is a bulk operation to create N amount of models passed in an array.
 	Create([]Model) ([]Model, error)
+	// Find writes on output the result of applying offset, limit and filters to find a range of models.
 	Find(output interface{}, offset, limit *int, filters ...Filter) error
+	// FindOne applies the given filters to write on entity the first result found.
 	FindOne(entity Model, filters ...Filter) error
+	// Update updates with the given data the different models that match filters.
 	Update(data interface{}, filters ...Filter) error
+	// Delete removes all the models that match filters.
 	Delete(filters ...Filter) error
+	// Model returns this repository's model.
 	Model() Model
 }
