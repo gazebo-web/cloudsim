0e852df21a5c74c0ca0389c7fa2fc259c568571e
Marcos Huck <marcoshuck@ekumenlabs.com>
2022-03-18 09:37:41 -0300

    Replace repository interface
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -1,14 +1,24 @@
 package repository
 
+import "errors"
+
+var (
+	// ErrNoFilter represents an error when no filter are provided.
+	ErrNoFilter = errors.New("no filters provided")
+	// ErrNoEntitiesUpdated represent an error when no entities were updated in the database
+	// after an Update operation.
+	ErrNoEntitiesUpdated = errors.New("no entities were updated")
+	// ErrNoEntitiesDeleted represent an error when no entities were deleted in the database
+	// after a Delete operation.
+	ErrNoEntitiesDeleted = errors.New("no entities were deleted")
+)
+
 // Repository holds methods to CRUD an entity on a certain persistence layer.
 type Repository interface {
-	// CreateBulk saves the given set of input entities in a persistence layer.
-	CreateBulk(in ...interface{}) error
-	// Get writes on out the data of the entity identified by id.
-	Get(out interface{}, id uint) error
-	// UpdateBulk overwrites the information given as the input on the different entities
-	// identified by the given ids.
-	UpdateBulk(in interface{}, ids ...uint) error
-	// RemoveBulk removes the entities identified by the given ids.
-	RemoveBulk(ids ...uint) error
+	Create([]Model) ([]Model, error)
+	Find(output interface{}, offset, limit *int, filters ...Filter) error
+	FindOne(entity Model, filters ...Filter) error
+	Update(data interface{}, filters ...Filter) error
+	Delete(filters ...Filter) error
+	Model() Model
 }
