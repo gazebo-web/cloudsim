3106093e0c0fa31cb7f877fe82b1523cbd3532fe
Marcos Huck <marcoshuck@ekumenlabs.com>
2022-03-21 14:44:23 -0300

    Remove repository package from cloudsim
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/repository/model.go
+++
@@ -1,7 +0,0 @@
-package repository
-
-// Model represents a generic entity. A Model is part of the domain layer and is  persisted by a certain Repository.
-type Model interface {
-	// TableName returns the table/collection name for a certain model.
-	TableName() string
-}
