2a7ff01a96b936970db45a100e3b514a9ca77215
Alejo Carballude <alejocarballude@gmail.com>
2022-04-20 17:09:55 -0400

    Make NewChownContainer apply recursively.
    Signed-off-by: Alejo Carballude <alejocarballude@gmail.com>

--- pkg/orchestrator/components/pods/utils.go
+++ pkg/orchestrator/components/pods/utils.go
@@ -3,12 +3,12 @@ package pods
 import "fmt"
 
 // NewChownContainer generates an init container that will be used to run the chown command in the /tmp folder.
-func NewChownContainer(volumes []Volume, path string, uid, gid uint) Container {
+func NewChownContainer(path string, uid, gid uint, volumes ...Volume) Container {
 	return Container{
 		Name:    "chown-shared-volume",
 		Image:   "infrastructureascode/aws-cli:latest",
 		Command: []string{"/bin/sh"},
-		Args:    []string{"-c", fmt.Sprintf("chown %d:%d %s", uid, gid, path)},
+		Args:    []string{"-c", fmt.Sprintf("chown -R %d:%d %s", uid, gid, path)},
 		Volumes: volumes,
 	}
 }
