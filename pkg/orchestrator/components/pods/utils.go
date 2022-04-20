c3496398bf2333b2d5560e45e3897d78f8c1e56b
Alejo Carballude <alejocarballude@gmail.com>
2022-04-20 16:59:41 -0400

    Allow specifying NewChownContainer target path, uid and gid.
    Signed-off-by: Alejo Carballude <alejocarballude@gmail.com>

--- pkg/orchestrator/components/pods/utils.go
+++ pkg/orchestrator/components/pods/utils.go
@@ -3,12 +3,12 @@ package pods
 import "fmt"
 
 // NewChownContainer generates an init container that will be used to run the chown command in the /tmp folder.
-func NewChownContainer(volumes []Volume) Container {
+func NewChownContainer(volumes []Volume, path string, uid, gid uint) Container {
 	return Container{
 		Name:    "chown-shared-volume",
 		Image:   "infrastructureascode/aws-cli:latest",
 		Command: []string{"/bin/sh"},
-		Args:    []string{"-c", fmt.Sprintf("chown %d:%d /tmp", 1000, 1000)},
+		Args:    []string{"-c", fmt.Sprintf("chown %d:%d %s", uid, gid, path)},
 		Volumes: volumes,
 	}
 }
