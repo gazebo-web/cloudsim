d87e65dde4907fe99b78bccb8644d557a46112b7
Alejo Carballude <alejocarballude@gmail.com>
2022-04-21 10:20:32 -0400

    Make NewChownContainer get paths from volumes. Update NewChownContainer documentation.
    Signed-off-by: Alejo Carballude <alejocarballude@gmail.com>

--- pkg/orchestrator/components/pods/utils.go
+++ pkg/orchestrator/components/pods/utils.go
@@ -1,14 +1,24 @@
 package pods
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-// NewChownContainer generates an init container that will be used to run the chown command in the /tmp folder.
-func NewChownContainer(path string, uid, gid uint, volumes ...Volume) Container {
+// NewChownContainer generates an init container that sets the owner of volume mount directories to a specific user
+// and group.
+func NewChownContainer(uid, gid uint, volumes ...Volume) Container {
+	mountPaths := make([]string, len(volumes))
+	for i, volume := range volumes {
+		mountPaths[i] = volume.Base().MountPath
+	}
+	paths := strings.Join(mountPaths, " ")
+
 	return Container{
 		Name:    "chown-shared-volume",
 		Image:   "infrastructureascode/aws-cli:latest",
 		Command: []string{"/bin/sh"},
-		Args:    []string{"-c", fmt.Sprintf("chown -R %d:%d %s", uid, gid, path)},
+		Args:    []string{"-c", fmt.Sprintf("chown -R %d:%d %s", uid, gid, paths)},
 		Volumes: volumes,
 	}
 }
