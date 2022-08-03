dbe41ee75098000a2e626ba240421c1ddb256b41
Marcos Huck <marcoshuck@ekumenlabs.com>
2022-08-03 13:01:40 -0300

    Remove Gloo factory from the ingress
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/orchestrator/components/ingresses/implementations/factory.go
+++ pkg/orchestrator/components/ingresses/implementations/factory.go
@@ -2,25 +2,20 @@ package implementations
 
 import (
 	factorymap "gitlab.com/ignitionrobotics/web/cloudsim/pkg/factory/map"
-	glooIngresses "gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator/components/ingresses/implementations/gloo/factory"
 	kubernetesIngresses "gitlab.com/ignitionrobotics/web/cloudsim/pkg/orchestrator/components/ingresses/implementations/kubernetes/factory"
 )
 
 const (
 	// Kubernetes is the Kubernetes implementation factory identifier.
 	Kubernetes = "kubernetes"
-	// Gloo is the Gloo implementation factory identifier.
-	Gloo = "gloo"
 )
 
 // IngressesFactory provides a factory to create Ingresses implementations.
 var IngressesFactory = factorymap.Map{
 	Kubernetes: kubernetesIngresses.IngressesNewFunc,
-	Gloo:       glooIngresses.IngressesNewFunc,
 }
 
 // IngressRulesFactory provides a factory to create IngressRules implementations.
 var IngressRulesFactory = factorymap.Map{
 	Kubernetes: kubernetesIngresses.IngressRulesNewFunc,
-	Gloo:       glooIngresses.IngressRulesNewFunc,
 }
